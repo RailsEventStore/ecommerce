4173f51d43d998f596e093a4cdcfcb134f3ba990
Mirosław Pragłowski <m@praglowski.com>
2015-04-30 17:13:45 +0200

    Rails conventions

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module CqrsEsSampleWithRes
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
