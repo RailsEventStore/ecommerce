31c2b1fea4db4f6b0b5c964ccc5daeca9b8bdbaa
Mirosław Pragłowski <m@praglowski.com>
2015-05-01 14:48:33 +0200

    Make sure a proper Order class is used

--- lib/denormalizers/router.rb
+++ lib/denormalizers/router.rb
@@ -2,10 +2,10 @@ module Denormalizers
   class Router
     def handle_event(event)
       case event.event_type
-      when 'OrderCreated' then Order.new.order_created(event)
-      when 'OrderExpired' then Order.new.order_created(event)
-      when 'ItemAddedToBasket'      then OrderLine.new.item_added_to_basket(event)
-      when 'ItemRemovedFromBasket'  then OrderLine.new.item_removed_from_basket(event)
+      when 'OrderCreated' then Denormalizers::Order.new.order_created(event)
+      when 'OrderExpired' then Denormalizers::Order.new.order_created(event)
+      when 'ItemAddedToBasket'      then Denormalizers::OrderLine.new.item_added_to_basket(event)
+      when 'ItemRemovedFromBasket'  then Denormalizers::OrderLine.new.item_removed_from_basket(event)
       end
     end
   end
