3bb8b278560c3f2e6de81edeee4ccf23a0855cb8
Mirosław Pragłowski <m@praglowski.com>
2017-09-19 22:10:14 +0200

    Use new way to subscribe to events by providing a class of handler

--- config/initializers/event_store.rb
+++ config/initializers/event_store.rb
@@ -1,6 +1,6 @@
 Rails.application.config.event_store.tap do |es|
-  es.subscribe(Denormalizers::OrderSubmitted.new, [Events::OrderSubmitted])
-  es.subscribe(Denormalizers::OrderExpired.new, [Events::OrderExpired])
-  es.subscribe(Denormalizers::ItemAddedToBasket.new, [Events::ItemAddedToBasket])
-  es.subscribe(Denormalizers::ItemRemovedFromBasket.new, [Events::ItemRemovedFromBasket])
+  es.subscribe(Denormalizers::OrderSubmitted, [Events::OrderSubmitted])
+  es.subscribe(Denormalizers::OrderExpired, [Events::OrderExpired])
+  es.subscribe(Denormalizers::ItemAddedToBasket, [Events::ItemAddedToBasket])
+  es.subscribe(Denormalizers::ItemRemovedFromBasket, [Events::ItemRemovedFromBasket])
 end
