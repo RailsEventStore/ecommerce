c4c2c595c2d11d053affda5ada3272845eff16b6
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-22 18:58:28 +0100

    Simpler browser setup is now possible.

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,7 @@ Rails.application.routes.draw do
   end
 
   require 'ruby_event_store/browser/app'
-  browser = ->(env) do
-    request = Rack::Request.new(env)
-    app = RubyEventStore::Browser::App.for(
-      event_store_locator: -> { Rails.configuration.event_store },
-      host: request.base_url,
-      path: request.script_name
-    )
-    app.call(env)
-  end
-  mount browser => '/res'
+  mount RubyEventStore::Browser::App.for(
+    event_store_locator: -> { Rails.configuration.event_store }
+  ) => '/res'
 end
