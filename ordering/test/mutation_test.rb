c5080c04afe980f3d66bd9bf9eeedd281a15fd73
Paweł Pacana <pawel.pacana@gmail.com>
2018-12-06 17:56:10 +0100

    Useless test.

--- ordering/test/mutation_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'minitest/autorun'
-require 'mutant/minitest/coverage'
-
-module Ordering
-  class MutationTest < ActiveSupport::TestCase
-    cover 'Ordering*'
-  end
-end
