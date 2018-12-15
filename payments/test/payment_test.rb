31625577f74108c043bffc660bc993eaf327823b
Mirosław Pragłowski <m@praglowski.com>
2018-12-15 13:06:13 +0100

    Refactor test

--- payments/test/payment_test.rb
+++ payments/test/payment_test.rb
@@ -6,9 +6,6 @@ module Payments
     cover 'Payments::Payment*'
 
     test 'authorize' do
-      transaction_id = SecureRandom.hex(16)
-      order_id = SecureRandom.uuid
-
       payment = Payment.new
       payment.authorize(transaction_id, order_id)
       assert_changes(payment.unpublished_events, [
@@ -20,20 +17,29 @@ module Payments
     end
 
     test 'should not allow for double authorization' do
-      transaction_id = SecureRandom.hex(16)
-      order_id = SecureRandom.uuid
-
-      payment = Payment.new
-      payment.apply(
-        PaymentAuthorized.new(data: {
-          transaction_id: transaction_id,
-          order_id: order_id,
-        })
-      )
-      assert_raises(AlreadyAuthorized) do
-        payment.authorize(transaction_id, order_id)
+      assert_raises(Payments::AlreadyAuthorized) do
+        authorized_payment.authorize(transaction_id, order_id)
       end
     end
 
+    private
+    def transaction_id
+      @transaction_id ||= SecureRandom.hex(16)
+    end
+
+    def order_id
+      @order_id ||= SecureRandom.uuid
+    end
+
+    def authorized_payment
+      Payment.new.tap do |payment|
+        payment.apply(
+          PaymentAuthorized.new(data: {
+            transaction_id: transaction_id,
+            order_id: order_id,
+          })
+        )
+      end
+    end
   end
 end
