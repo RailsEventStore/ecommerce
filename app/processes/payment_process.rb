3619a6db1fddf123f3ab856efbfff9a4bb0e23a0
Mirosław Pragłowski <m@praglowski.com>
2018-12-17 00:45:10 +0100

    Payment process manager

---
+++ app/processes/payment_process.rb
@@ -0,0 +1,59 @@
+class PaymentProcess
+  def initialize(store: Rails.configuration.event_store,
+                 bus: Rails.configuration.command_bus)
+    @store = store
+    @bus = bus
+  end
+
+  def call(event)
+    state = build_state(event)
+    if state.release?
+      bus.call(Payments::ReleasePayment.new(
+        order_id: state.order_id,
+        transaction_id: state.transaction_id))
+    end
+  end
+
+  private
+  attr_reader :store, :bus
+
+  def build_state(event)
+    stream_name = "PaymentProcess$#{event.data.fetch(:order_id)}"
+    past = store.read.stream(stream_name).to_a
+    last_stored = past.size - 1
+    store.link(event.event_id, stream_name: stream_name, expected_version: last_stored)
+    ProcessState.new.tap do |state|
+      past.each{|ev| state.call(ev)}
+      state.call(event)
+    end
+  end
+
+  class ProcessState
+    def initialize
+      @order = :draft
+      @payment = :none
+    end
+    attr_reader :transaction_id, :order_id
+
+    def call(event)
+      case event
+      when Payments::PaymentAuthorized
+        @payment = :authorized
+        @transaction_id = event.data.fetch(:transaction_id)
+      when Payments::PaymentReleased
+        @payment = :released
+      when Ordering::OrderSubmitted
+        @order = :submitted
+        @order_id = event.data.fetch(:order_id)
+      when Ordering::OrderExpired
+        @order = :expired
+      when Ordering::OrderPaid
+        @order = :paid
+      end
+    end
+
+    def release?
+      @payment == :authorized && @order == :expired
+    end
+  end
+end
---
+++ test/processes/payment_process_test.rb
@@ -0,0 +1,113 @@
+require 'test_helper'
+
+class PaymentProcessTest < ActiveSupport::TestCase
+  test 'happy path' do
+    fake = FakeCommandBus.new
+    process = PaymentProcess.new(bus: fake)
+    given([
+      order_submitted,
+      payment_authorized,
+      order_paid
+    ]).each do |event|
+      process.call(event)
+    end
+    assert_nil(fake.received)
+  end
+
+  test 'order expired without payment' do
+    fake = FakeCommandBus.new
+    process = PaymentProcess.new(bus: fake)
+    given([
+      order_submitted,
+      order_expired,
+    ]).each do |event|
+      process.call(event)
+    end
+    assert_nil(fake.received)
+  end
+
+  test 'order expired after payment authorization' do
+    fake = FakeCommandBus.new
+    process = PaymentProcess.new(bus: fake)
+    given([
+      order_submitted,
+      payment_authorized,
+      order_expired,
+    ]).each do |event|
+      process.call(event)
+    end
+    assert_equal(fake.received,
+      Payments::ReleasePayment.new(transaction_id: transaction_id)
+    )
+  end
+
+  test 'order expired after payment released' do
+    fake = FakeCommandBus.new
+    process = PaymentProcess.new(bus: fake)
+    given([
+      order_submitted,
+      payment_authorized,
+      payment_released,
+      order_expired,
+    ]).each do |event|
+      process.call(event)
+    end
+    assert_nil(fake.received)
+  end
+
+  private
+
+  class FakeCommandBus
+    attr_reader :received
+    def call(command)
+      @received = command
+    end
+  end
+
+  def transaction_id
+    @transaction_id ||= SecureRandom.hex(16)
+  end
+
+  def order_id
+    @order_id ||= SecureRandom.uuid
+  end
+
+  def order_number
+    '2018/12/16/1'
+  end
+
+  def customer_id
+    123
+  end
+
+  def given(events, store: Rails.configuration.event_store)
+    events.each{|ev| store.append(ev)}
+    events
+  end
+
+  def order_submitted
+    Ordering::OrderSubmitted.new(data: {order_id: order_id, order_number: order_number, customer_id: customer_id})
+  end
+
+  def order_expired
+    Ordering::OrderExpired.new(data: {order_id: order_id})
+  end
+
+  def order_paid
+    Ordering::OrderPaid.new(data: {order_id: order_id, transaction_id: transaction_id})
+  end
+
+  def payment_authorized
+    Payments::PaymentAuthorized.new(data: {
+      transaction_id: transaction_id,
+      order_id: order_id
+    })
+  end
+
+  def payment_released
+    Payments::PaymentReleased.new(data: {
+      transaction_id: transaction_id,
+      order_id: order_id
+    })
+  end
+end
