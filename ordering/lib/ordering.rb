a48db73df55b55b52cc31eef80f99100de4ead4f
Paweł Pacana <pawel.pacana@gmail.com>
2018-12-28 17:28:10 +0100

    Revert "Remove unused require_dependency statements"
    This reverts commit 5300bf98fbed80526f23f4de2e946c09aa916fcc.
    
    For rationale see:
    https://github.com/RailsEventStore/cqrs-es-sample-with-res/commit/e7de7e842e4c75b04292771d22cc86544e9de29a

--- ordering/lib/ordering.rb
+++ ordering/lib/ordering.rb
@@ -1,2 +1,22 @@
 module Ordering
 end
+
+require_dependency 'ordering/add_item_to_basket'
+require_dependency 'ordering/fake_number_generator'
+require_dependency 'ordering/item_added_to_basket'
+require_dependency 'ordering/item_removed_from_basket'
+require_dependency 'ordering/number_generator'
+require_dependency 'ordering/on_add_item_to_basket'
+require_dependency 'ordering/on_remove_item_from_basket'
+require_dependency 'ordering/on_set_order_as_expired'
+require_dependency 'ordering/on_mark_order_as_paid'
+require_dependency 'ordering/on_submit_order'
+require_dependency 'ordering/order'
+require_dependency 'ordering/order_expired'
+require_dependency 'ordering/order_paid'
+require_dependency 'ordering/order_line'
+require_dependency 'ordering/order_submitted'
+require_dependency 'ordering/remove_item_from_basket'
+require_dependency 'ordering/set_order_as_expired'
+require_dependency 'ordering/mark_order_as_paid'
+require_dependency 'ordering/submit_order'
--- payments/lib/payments.rb
+++ payments/lib/payments.rb
@@ -1,2 +1,7 @@
 module Payments
 end
+
+require_dependency 'payments/payment_authorized'
+require_dependency 'payments/payment_captured'
+require_dependency 'payments/payment_released'
+require_dependency 'payments/payment'
