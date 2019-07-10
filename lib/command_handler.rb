e4983433bc5d71252da58a23da9374f17e1d5cb3
Paweł Pacana <pawel.pacana@gmail.com>
2019-07-10 13:25:23 +0200

    Instrument AggregateRoot.
    This allows subscribing for load and store notifications.

--- lib/command_handler.rb
+++ lib/command_handler.rb
@@ -1,13 +1,13 @@
 module CommandHandler
   def with_aggregate(aggregate_class, aggregate_id, &block)
-    repository = AggregateRoot::Repository.new(Rails.configuration.event_store)
+    repository = AggregateRoot::InstrumentedRepository.new(AggregateRoot::Repository.new(Rails.configuration.event_store), ActiveSupport::Notifications)
     aggregate = aggregate_class.new(aggregate_id)
     stream = stream_name(aggregate_class, aggregate_id)
     repository.with_aggregate(aggregate, stream, &block)
   end
 
   def rehydrate(aggregate, stream)
-    repository = AggregateRoot::Repository.new(Rails.configuration.event_store)
+    repository = AggregateRoot::InstrumentedRepository.new(AggregateRoot::Repository.new(Rails.configuration.event_store), ActiveSupport::Notifications)
     repository.load(aggregate, stream)
   end
 
