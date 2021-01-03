681d8639c421d9b1671055d4b8a5a0cbc8c495e4
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-03 11:58:47 +0100

    Port event implementation to 2.0
    https://github.com/RailsEventStore/rails_event_store/blob/master/railseventstore.org/source/docs/v2/custom_components_2_0.html.md

--- lib/event.rb
+++ lib/event.rb
@@ -4,8 +4,7 @@ class Event < Dry::Struct
   transform_keys(&:to_sym)
 
   def self.new(data: {}, metadata: {}, **rest)
-    timestamp = Time.parse(metadata.delete(:timestamp)) rescue nil
-    super(rest.merge(data).merge(metadata: metadata.merge(timestamp: timestamp)))
+    super(rest.merge(data).merge(metadata: metadata))
   end
 
   def self.inherited(klass)
@@ -14,20 +13,16 @@ class Event < Dry::Struct
     klass.attribute :event_id, Types::UUID.default { SecureRandom.uuid }
   end
 
-  def to_h
-    {
-      event_id: event_id,
-      metadata: metadata,
-      data:     super.except(:event_id, :metadata)
-    }
-  end
-
   def timestamp
     metadata[:timestamp]
   end
 
+  def valid_at
+    metadata[:valid_at]
+  end
+
   def data
-    to_h[:data]
+    to_h.except(:event_id, :metadata)
   end
 
   def event_type
