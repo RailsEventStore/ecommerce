35a4cf4fa1b6c06f01e9666af5be3a035fec4a7c
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-03 12:19:42 +0100

    Whatever

--- lib/types.rb
+++ lib/types.rb
@@ -1,7 +1,7 @@
 require 'dry-types'
 
 module Types
-  include Dry::Types.module
+  include Dry::Types()
 
   UUID          = Types::Strict::String.constrained(format: /\A[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}\z/i)
   ID            = Types::Strict::Integer
