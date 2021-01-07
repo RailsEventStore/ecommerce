2f287af9730fee9ca38ccd3166930fb94f1cf604
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-07 19:45:42 +0100

    Extract subscribe/register glue to Configuration

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -1,38 +1,16 @@
 require 'rails_event_store'
-require 'aggregate_root'
 require 'arkency/command_bus'
 
 
 Rails.configuration.to_prepare do
-  Rails.configuration.event_store = RailsEventStore::Client.new(
-    repository: RailsEventStoreActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL)
+  repository =
+    RailsEventStoreActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL)
+  Rails.configuration.event_store =
+    RailsEventStore::Client.new(repository: repository)
+  Rails.configuration.command_bus =
+    Arkency::CommandBus.new
+  Configuration.new.call(
+    Rails.configuration.event_store,
+    Rails.configuration.command_bus
   )
-  Rails.configuration.command_bus = Arkency::CommandBus.new
-
-  Rails.configuration.event_store.tap do |store|
-    store.subscribe(Orders::OnOrderSubmitted, to: [Ordering::OrderSubmitted])
-    store.subscribe(Orders::OnOrderExpired, to: [Ordering::OrderExpired])
-    store.subscribe(Orders::OnOrderPaid, to: [Ordering::OrderPaid])
-    store.subscribe(Orders::OnItemAddedToBasket, to: [Ordering::ItemAddedToBasket])
-    store.subscribe(Orders::OnItemRemovedFromBasket, to: [Ordering::ItemRemovedFromBasket])
-
-    store.subscribe(PaymentProcess, to: [
-      Ordering::OrderSubmitted,
-      Ordering::OrderExpired,
-      Ordering::OrderPaid,
-      Payments::PaymentAuthorized,
-      Payments::PaymentReleased,
-    ])
-  end
-
-  Rails.configuration.command_bus.tap do |bus|
-    bus.register(Ordering::SubmitOrder, Ordering::OnSubmitOrder.new(number_generator: Rails.configuration.number_generator.call))
-    bus.register(Ordering::SetOrderAsExpired, Ordering::OnSetOrderAsExpired.new)
-    bus.register(Ordering::MarkOrderAsPaid, Ordering::OnMarkOrderAsPaid.new)
-    bus.register(Ordering::AddItemToBasket, Ordering::OnAddItemToBasket.new)
-    bus.register(Ordering::RemoveItemFromBasket, Ordering::OnRemoveItemFromBasket.new)
-    bus.register(Payments::AuthorizePayment, Payments::OnAuthorizePayment.new)
-    bus.register(Payments::CapturePayment, Payments::OnCapturePayment.new)
-    bus.register(Payments::ReleasePayment, Payments::OnReleasePayment.new)
-  end
 end
---
+++ lib/configuration.rb
@@ -0,0 +1,26 @@
+class Configuration
+  def call(event_store, command_bus)
+    event_store.subscribe(Orders::OnOrderSubmitted, to: [Ordering::OrderSubmitted])
+    event_store.subscribe(Orders::OnOrderExpired, to: [Ordering::OrderExpired])
+    event_store.subscribe(Orders::OnOrderPaid, to: [Ordering::OrderPaid])
+    event_store.subscribe(Orders::OnItemAddedToBasket, to: [Ordering::ItemAddedToBasket])
+    event_store.subscribe(Orders::OnItemRemovedFromBasket, to: [Ordering::ItemRemovedFromBasket])
+
+    event_store.subscribe(PaymentProcess, to: [
+      Ordering::OrderSubmitted,
+      Ordering::OrderExpired,
+      Ordering::OrderPaid,
+      Payments::PaymentAuthorized,
+      Payments::PaymentReleased,
+    ])
+
+    command_bus.register(Ordering::SubmitOrder, Ordering::OnSubmitOrder.new(number_generator: Rails.configuration.number_generator.call))
+    command_bus.register(Ordering::SetOrderAsExpired, Ordering::OnSetOrderAsExpired.new)
+    command_bus.register(Ordering::MarkOrderAsPaid, Ordering::OnMarkOrderAsPaid.new)
+    command_bus.register(Ordering::AddItemToBasket, Ordering::OnAddItemToBasket.new)
+    command_bus.register(Ordering::RemoveItemFromBasket, Ordering::OnRemoveItemFromBasket.new)
+    command_bus.register(Payments::AuthorizePayment, Payments::OnAuthorizePayment.new)
+    command_bus.register(Payments::CapturePayment, Payments::OnCapturePayment.new)
+    command_bus.register(Payments::ReleasePayment, Payments::OnReleasePayment.new)
+  end
+end
