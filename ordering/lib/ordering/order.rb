71be3b282eaa1bae368b3dc5d065dd22ed00be0e
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-03-17 23:51:12 +0100

    Removed dead code - OrderLine
    It managed an internal state in Order which was
    never used (and ignored by mutant).

--- ordering/lib/ordering/order.rb
+++ ordering/lib/ordering/order.rb
@@ -11,7 +11,6 @@ module Ordering
     def initialize(id)
       @id = id
       @state = :draft
-      @order_lines = []
     end
 
     def submit(order_number, customer_id)
@@ -67,35 +66,9 @@ module Ordering
     end
 
     on ItemAddedToBasket do |event|
-      product_id = event.data[:product_id]
-      order_line = find_order_line(product_id)
-      unless order_line
-        order_line = create_order_line(product_id)
-        @order_lines << order_line
-      end
-      order_line.increase_quantity
     end
 
     on ItemRemovedFromBasket do |event|
-      product_id = event.data[:product_id]
-      order_line = find_order_line(product_id)
-      return unless order_line
-      order_line.decrease_quantity
-      remove_order_line(order_line) if order_line.empty?
-    end
-
-    private
-
-    def find_order_line(product_id)
-      @order_lines.select { |line| line.product_id == product_id }.first
-    end
-
-    def create_order_line(product_id)
-      OrderLine.new(product_id)
-    end
-
-    def remove_order_line(order_line)
-      @order_lines.delete(order_line)
     end
   end
 end
