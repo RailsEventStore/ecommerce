0ddff66fc790fe9bbc45ec5a21e526b4d576f086
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-03-17 23:01:00 +0100

    Fixed test, we now confirm order after payment
    Previously, the payments were not connected to
    the order. Now, we require a correct order setup.

--- payments/test/on_capture_payment_test.rb
+++ payments/test/on_capture_payment_test.rb
@@ -10,8 +10,11 @@ module Payments
       transaction_id = SecureRandom.hex(16)
       order_id = SecureRandom.uuid
       stream = "Payments::Payment$#{transaction_id}"
-
+      product  = Product.create(name: 'test')
+      customer = Customer.create(name: 'test')
       arrange(
+        Ordering::AddItemToBasket.new(order_id: order_id, product_id: product.id),
+        Ordering::SubmitOrder.new(order_id: order_id, customer_id: customer.id),
         AuthorizePayment.new(transaction_id: transaction_id, order_id: order_id)
       )
 
