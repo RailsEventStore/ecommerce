fba1ff1f1dc3fdfc84a605ef29db5289bacb5f89
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-03-25 09:23:46 +0100

    Moved cmd handlers to one file ordering.rb
    It's mostly subjective, but now that we're going to change many cmd handlers at once (when we move logic to Cart) it feels this belongs together.
    
    Zeitwerk allows it, which is cool.
    
    Potential next steps here could be making this one class instead of multiple classes. However, this would require wrapping the method calls in command handlers configuration with lambdas.

--- ordering/lib/ordering.rb
+++ ordering/lib/ordering.rb
@@ -1,3 +1,71 @@
 module Ordering
+  class OnAddItemToBasket
+    include CommandHandler
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order.add_item(command.product_id)
+      end
+    end
+  end
+
+  class OnCancelOrder
+    include CommandHandler
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order.cancel
+      end
+    end
+  end
+
+  class OnMarkOrderAsPaid
+    include CommandHandler
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order.confirm(command.transaction_id)
+      end
+    end
+  end
+
+  class OnRemoveItemFromBasket
+    include CommandHandler
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order.remove_item(command.product_id)
+      end
+    end
+  end
+  class OnSetOrderAsExpired
+    include CommandHandler
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order.expire
+      end
+    end
+  end
+  class OnSubmitOrder
+    include CommandHandler
+
+    def initialize(number_generator:)
+      @number_generator = number_generator
+    end
+
+    def call(command)
+      with_aggregate(Order, command.aggregate_id) do |order|
+        order_number = number_generator.call
+        order.submit(order_number, command.customer_id)
+      end
+    end
+
+    private
+
+    attr_accessor :number_generator
+  end
 end
+
+
 
