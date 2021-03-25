73e2d5ff9f03898a7d3f7c1c20c4095fc1dc46b7
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-03-25 09:13:32 +0100

    Back to green mutant.
    Previous attempt to Order/Cart refactoring was based on the strategy of duplicated flow for the time being. But it had other drawbacks, mutation coverage being just a minor problem.
    
    The incorrect assumption was to reuse card is as order id. This could make the transition easier but it led to artificial testing.

--- ordering/lib/ordering/cmd_handlers/on_add_item_to_basket.rb
+++ ordering/lib/ordering/cmd_handlers/on_add_item_to_basket.rb
@@ -6,10 +6,6 @@ module Ordering
       with_aggregate(Order, command.aggregate_id) do |order|
         order.add_item(command.product_id)
       end
-
-      with_aggregate(::Ecommerce::Cart, command.aggregate_id) do |cart|
-        cart.add_item(command.product_id)
-      end
     end
   end
 end
