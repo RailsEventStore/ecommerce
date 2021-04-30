e8904c7e967df0deadacc37120bbd3719f311581
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-04-30 13:38:41 +0200

    NumberGenerator tests
    This was my attempt to eliminate the mutant ignore subject on this class. But testing randomness is tricky, so I'm just leaving at least some coverage here. The mutant stays with ignoring this.

--- ordering/lib/ordering/number_generator.rb
+++ ordering/lib/ordering/number_generator.rb
@@ -1,7 +1,13 @@
 module Ordering
   class NumberGenerator
     def call
-      Time.current.strftime("%Y/%m/#{SecureRandom.random_number(100)}")
+      Time.current.strftime("%Y/%m/#{random_number}")
+    end
+
+    private
+
+    def random_number
+      SecureRandom.random_number(100)
     end
   end
 end
---
+++ ordering/test/number_generator_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+module Ordering
+  class NumberGeneratorTest < ActiveSupport::TestCase
+    def test_includes_year
+      assert_includes(NumberGenerator.new.call, "#{Time.current.year}")
+    end
+
+    def test_includes_month
+      assert_includes(NumberGenerator.new.call, "#{Time.current.month}")
+    end
+  end
+end
