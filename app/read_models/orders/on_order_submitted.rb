8a0e5545ba4951b15262d9b7931bb7b70605613f
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-05-16 21:25:52 +0200

    Fixed bug with crashing when submitting a new order
    This hotfix allows submitting an empty order.
    
    This is debatable whether it's a good fix or not. For now, it's better than a crash.

--- app/read_models/orders/on_order_submitted.rb
+++ app/read_models/orders/on_order_submitted.rb
@@ -1,7 +1,7 @@
 module Orders
   class OnOrderSubmitted
     def call(event)
-      order = Order.find_by(uid: event.data[:order_id])
+      order = Order.find_or_create_by(uid: event.data[:order_id])
       order.number = event.data[:order_number]
       order.customer = Customer.find(event.data[:customer_id]).name
       order.state = "Submitted"
---
+++ test/integration/orders_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  def test_submitting_empty_order
+    get "/"
+    assert_select "h1", "Orders"
+    get "/orders/new"
+    assert_select "h1", "New Order"
+    post "/orders", params:
+         {
+           "authenticity_token"=>"[FILTERED]",
+           "order_id"=>"288c590d-b7dc-429f-8d82-79ebf2d5aabc",
+           "customer_id"=>"1",
+           "commit"=>"Submit order"
+         }
+  end
+end
