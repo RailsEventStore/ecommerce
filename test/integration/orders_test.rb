436f27257ffbf0f267c179840bbf1cc6a9289521
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-05-17 11:00:02 +0200

    Use the created customer id instead of hardcoded one

--- test/integration/orders_test.rb
+++ test/integration/orders_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class BlogFlowTest < ActionDispatch::IntegrationTest
 
   def test_submitting_empty_order
-    Customer.create(name: "Arkency")
+    arkency = Customer.create(name: "Arkency")
+
     get "/"
     assert_select "h1", "Orders"
     get "/orders/new"
@@ -12,7 +13,7 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
          {
            "authenticity_token"=>"[FILTERED]",
            "order_id"=>"288c590d-b7dc-429f-8d82-79ebf2d5aabc",
-           "customer_id"=>"1",
+           "customer_id"=>arkency.id,
            "commit"=>"Submit order"
          }
   end
