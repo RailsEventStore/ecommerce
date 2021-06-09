d49da833f168c6cfa246ffadf202e56c2e382e0b
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-06-09 15:55:34 +0200

    Let's make order lines order explicit in association

--- app/read_models/orders/order.rb
+++ app/read_models/orders/order.rb
@@ -3,6 +3,7 @@ module Orders
     self.table_name = "orders"
 
     has_many :order_lines,
+             -> { order(id: :asc) },
              class_name: "Orders::OrderLine",
              foreign_key: :order_uid,
              primary_key: :uid
