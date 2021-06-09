a9be4b1c947eee8f5178dc2c51482add28670089
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-06-09 15:12:26 +0200

    Added a new assertion to help debug a testing issue

--- test/orders/item_added_to_basket_test.rb
+++ test/orders/item_added_to_basket_test.rb
@@ -65,6 +65,7 @@ module Orders
       order = Orders::Order.find_by(uid: order_id)
       assert_equal(order.order_lines.count, 2)
       order_lines = order.order_lines
+      assert_equal([product.id, another_product.id], order_lines.map(&:product_id))
       assert_equal(order_lines[0].product_id, product.id)
       assert_equal(order_lines[0].product_name, 'something')
       assert_equal(order_lines[0].quantity , 1)
