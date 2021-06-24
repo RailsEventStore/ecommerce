42a72e3922f8136871170dac85d804c121335043
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-06-24 11:44:35 +0200

    Local script to reset heroku database
    At the moment, we don't want to care about data compatibility so it's the simplest to reset database whenever we changed any schema etc
    
    https://gist.github.com/zulhfreelancer/ea140d8ef9292fa9165e

---
+++ bin/reset_heroku_db.sh
@@ -0,0 +1,4 @@
+heroku restart --app=cqrs-es-sample-with-res
+heroku pg:reset DATABASE --app=cqrs-es-sample-with-res --confirm cqrs-es-sample-with-res
+heroku run rake db:migrate --app=cqrs-es-sample-with-res
+heroku run rake db:seed --app=cqrs-es-sample-with-res
