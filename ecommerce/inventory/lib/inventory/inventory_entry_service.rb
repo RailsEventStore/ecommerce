f649c87fe28194c636639ad872087656806f0a65
pjurewicz <5s7z68c8>
2021-08-25 21:43:47 +0200

    Simplified InventoryEntryService

--- ecommerce/inventory/lib/inventory/inventory_entry_service.rb
+++ ecommerce/inventory/lib/inventory/inventory_entry_service.rb
@@ -14,31 +14,6 @@ module Inventory
       end
     end
 
-    def dispatch(command)
-      with_inventory_entry(command.product_id) do |entry|
-        entry.release(command.quantity)
-        entry.dispatch(command.quantity)
-      end
-    end
-
-    def reserve_stock(command)
-      with_inventory_entry(command.product_id) do |entry|
-        entry.reserve(command.quantity)
-      end
-    end
-
-    def release_stock(command)
-      with_inventory_entry(command.product_id) do |entry|
-        entry.release(command.quantity)
-      end
-    end
-
-    def check_availability(command)
-      with_inventory_entry(command.product_id) do |entry|
-        entry.check_availability(command.quantity)
-      end
-    end
-
     def with_inventory_entry(product_id)
       with_aggregate(InventoryEntry, product_id) do |entry|
         yield(entry)
