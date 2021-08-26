21bfc1b1fc51bc57e9269322d5df1afcedd4acf1
pjurewicz <5s7z68c8>
2021-08-26 13:57:29 +0200

    Added integration test to ensure full mutant coverage. It checks whether order submission is rolled back when InventoryNotAvailable error occurs.

---
+++ rails_application/test/integration/ordering_inventory_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class OrderingInventoryTest < Ecommerce::RealRESIntegrationTestCase
+  include TestPlumbing
+
+  cover 'Ordering::OnSubmitOrder*'
+
+  def test_inventory_error_prevents_order_submission
+    aggregate_id = SecureRandom.uuid
+    customer_id = SecureRandom.uuid
+    product_id = SecureRandom.uuid
+
+    arrange(
+      Crm::RegisterCustomer.new(customer_id: customer_id, name: 'test'),
+      ProductCatalog::RegisterProduct.new(product_id: product_id, name: "Async Remote"),
+      Pricing::SetPrice.new(product_id: product_id, price: 39),
+      Inventory::Supply.new(product_id: product_id, quantity: 1),
+      Pricing::AddItemToBasket.new(order_id: aggregate_id, product_id: product_id),
+      Pricing::AddItemToBasket.new(order_id: aggregate_id, product_id: product_id)
+    )
+    stream = "Ordering::Order$#{aggregate_id}"
+    assert_events(stream) do
+      assert_raises(Inventory::InventoryEntry::InventoryNotAvailable) do
+        act(
+          Ordering::SubmitOrder.new(order_id: aggregate_id, customer_id: customer_id)
+        )
+      end
+    end
+  end
+end
