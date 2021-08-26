9eb12c96f00be0c74aef3640d586910f2da52d56
pjurewicz <5s7z68c8>
2021-08-26 11:49:52 +0200

    Simplified InventoryTest

--- ecommerce/inventory/test/inventory_test.rb
+++ ecommerce/inventory/test/inventory_test.rb
@@ -8,8 +8,8 @@ module Inventory
 
     def test_if_stock_level_changes_with_supply_command
       product_id = SecureRandom.uuid
-      stream = "Inventory::InventoryEntry$#{product_id}"
-      assert_events(stream, StockLevelChanged.new(data: { product_id: product_id, quantity: 1, stock_level: 1 })) do
+      assert_events(inventory_entry_stream(product_id),
+                    StockLevelChanged.new(data: { product_id: product_id, quantity: 1, stock_level: 1 })) do
         supply(product_id, 1)
       end
     end
@@ -19,11 +19,10 @@ module Inventory
       register_product(product_id, "test")
       set_price(product_id, 20)
       order_id = SecureRandom.uuid
-      stream = "Inventory::Reservation$#{order_id}"
-      assert_events(stream, ReservationAdjusted.new(data: { product_id: product_id, quantity: 1 })) do
+      assert_events(reservation_stream(order_id), ReservationAdjusted.new(data: { order_id: order_id, product_id: product_id, quantity: 1 })) do
         add_item(order_id, product_id)
       end
-      assert_events(stream, ReservationAdjusted.new(data: { product_id: product_id, quantity: -1 })) do
+      assert_events(reservation_stream(order_id), ReservationAdjusted.new(data: { order_id: order_id, product_id: product_id, quantity: -1 })) do
         remove_item(order_id, product_id)
       end
     end
@@ -38,10 +37,8 @@ module Inventory
       customer_id = SecureRandom.uuid
       register_customer(customer_id, 'test')
 
-      reservation_stream = "Inventory::Reservation$#{order_id}"
-      product_stream = "Inventory::InventoryEntry$#{product_id}"
-      assert_events(reservation_stream, ReservationSubmitted.new(data: { reservation_items: [product_id: product_id, quantity: 1] })) do
-        assert_events(product_stream, StockReserved.new(data: { product_id: product_id, quantity: 1 })) do
+      assert_events(reservation_stream(order_id), ReservationSubmitted.new(data: { order_id: order_id, reservation_items: [product_id: product_id, quantity: 1] })) do
+        assert_events(inventory_entry_stream(product_id), StockReserved.new(data: { product_id: product_id, quantity: 1 })) do
           submit_order(order_id, customer_id)
         end
       end
@@ -58,10 +55,8 @@ module Inventory
       register_customer(customer_id, 'test')
       submit_order(order_id, customer_id)
 
-      reservation_stream = "Inventory::Reservation$#{order_id}"
-      product_stream = "Inventory::InventoryEntry$#{product_id}"
-      assert_events(reservation_stream, ReservationCanceled.new(data: { reservation_items: [product_id: product_id, quantity: 1] })) do
-        assert_events(product_stream, StockReleased.new(data: { product_id: product_id, quantity: 1 })) do
+      assert_events(reservation_stream(order_id), ReservationCanceled.new(data: { order_id: order_id, reservation_items: [product_id: product_id, quantity: 1] })) do
+        assert_events(inventory_entry_stream(product_id), StockReleased.new(data: { product_id: product_id, quantity: 1 })) do
           cancel_order(order_id)
         end
       end
@@ -78,15 +73,15 @@ module Inventory
       register_customer(customer_id, 'test')
       submit_order(order_id, customer_id)
 
-      reservation_stream = "Inventory::Reservation$#{order_id}"
-      product_stream = "Inventory::InventoryEntry$#{product_id}"
-      assert_events(reservation_stream, ReservationCompleted.new(data: { reservation_items: [product_id: product_id, quantity: 1] })) do
-        assert_events(product_stream,
+      assert_events(reservation_stream(order_id),
+                    ReservationCompleted.new(data: { order_id: order_id, reservation_items: [product_id: product_id, quantity: 1] })) do
+        assert_events(inventory_entry_stream(product_id),
                       StockReleased.new(data: { product_id: product_id, quantity: 1 }),
                       StockLevelChanged.new(data: { product_id: product_id, quantity: -1, stock_level: 0 })) do
           confirm_order(order_id)
         end
       end
+
     end
 
     def test_if_prevents_from_order_submission_when_out_of_stock
@@ -98,6 +93,27 @@ module Inventory
       2.times { add_item(order_id, product_id) }
       customer_id = SecureRandom.uuid
       register_customer(customer_id, 'test')
+
+      assert_raises(Inventory::InventoryEntry::InventoryNotAvailable) do
+        submit_order(order_id, customer_id)
+      end
+    end
+
+    def test_if_prevents_from_order_submission_when_stock_reserved
+      product_id = SecureRandom.uuid
+      register_product(product_id, "test")
+      set_price(product_id, 20)
+      supply(product_id, 1)
+
+      order_id = SecureRandom.uuid
+      add_item(order_id, product_id)
+      customer_id = SecureRandom.uuid
+      register_customer(customer_id, 'test')
+      submit_order(order_id, customer_id)
+
+      order_id = SecureRandom.uuid
+      add_item(order_id, product_id)
+
       assert_raises(Inventory::InventoryEntry::InventoryNotAvailable) do
         submit_order(order_id, customer_id)
       end
@@ -112,9 +128,8 @@ module Inventory
       customer_id = SecureRandom.uuid
       register_customer(customer_id, 'test')
 
-      product_stream = "Inventory::InventoryEntry$#{product_id}"
       assert_nothing_raised do
-        assert_events(product_stream) do
+        assert_events(inventory_entry_stream(product_id)) do
           submit_order(order_id, customer_id)
         end
       end
@@ -122,40 +137,52 @@ module Inventory
 
     private
 
+    def order_stream order_id
+      "Ordering::Order$#{order_id}"
+    end
+
+    def inventory_entry_stream product_id
+      "Inventory::InventoryEntry$#{product_id}"
+    end
+
+    def reservation_stream order_id
+      "Inventory::Reservation$#{order_id}"
+    end
+
     def register_product(product_id, product_name)
-      run_command(ProductCatalog::RegisterProduct.new(product_id: product_id, name: name))
+      act(ProductCatalog::RegisterProduct.new(product_id: product_id, name: name))
     end
 
     def set_price(product_id, amount)
-      run_command(Pricing::SetPrice.new(product_id: product_id, price: amount))
+      act(Pricing::SetPrice.new(product_id: product_id, price: amount))
     end
 
     def add_item(order_id, product_id)
-      run_command(Pricing::AddItemToBasket.new(order_id: order_id, product_id: product_id))
+      act(Pricing::AddItemToBasket.new(order_id: order_id, product_id: product_id))
     end
 
     def remove_item(order_id, product_id)
-      run_command(Pricing::RemoveItemFromBasket.new(order_id: order_id, product_id: product_id))
+      act(Pricing::RemoveItemFromBasket.new(order_id: order_id, product_id: product_id))
     end
 
     def submit_order(order_id, customer_id)
-      run_command(Ordering::SubmitOrder.new(order_id: order_id, customer_id: customer_id))
+      act(Ordering::SubmitOrder.new(order_id: order_id, customer_id: customer_id))
     end
 
     def register_customer(customer_id, name)
-      run_command(Crm::RegisterCustomer.new(customer_id: customer_id, name: name))
+      act(Crm::RegisterCustomer.new(customer_id: customer_id, name: name))
     end
 
     def supply(product_id, quantity)
-      run_command(Supply.new(product_id: product_id, quantity: quantity))
+      act(Supply.new(product_id: product_id, quantity: quantity))
     end
 
     def cancel_order(order_id)
-      run_command(Ordering::CancelOrder.new(order_id: order_id))
+      act(Ordering::CancelOrder.new(order_id: order_id))
     end
 
     def confirm_order(order_id)
-      run_command(Ordering::MarkOrderAsPaid.new(order_id: order_id))
+      act(Ordering::MarkOrderAsPaid.new(order_id: order_id))
     end
   end
 end
