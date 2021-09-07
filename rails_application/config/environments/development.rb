8aa0c5e8774be4402301d85453bfd3aa198b0cd4
Paweł Pacana <pawel.pacana@gmail.com>
2021-09-07 12:48:44 +0200

    Allow basic puma-dev domain

--- rails_application/config/environments/development.rb
+++ rails_application/config/environments/development.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << ".test"
+
   # --- CqrsEsSampleWithRes ---
   config.number_generator = ->{ Ordering::NumberGenerator.new }
   config.payment_gateway = -> { @gateway ||= Payments::FakeGateway.new }
