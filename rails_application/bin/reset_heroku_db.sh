ce6394d42320baf7a7bdd0f18371f60e03ae539b
Paweł Pacana <pawel.pacana@gmail.com>
2021-09-07 12:32:41 +0200

    Improve recognition of script type

--- rails_application/bin/reset_heroku_db.sh
+++ rails_application/bin/reset_heroku_db.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 heroku restart --app=cqrs-es-sample-with-res
 heroku pg:reset DATABASE --app=cqrs-es-sample-with-res --confirm cqrs-es-sample-with-res
 heroku run rake db:migrate --app=cqrs-es-sample-with-res
