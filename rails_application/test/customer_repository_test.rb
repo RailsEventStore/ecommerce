8d59ffe8340a8e832113d90c27507103b64f4c7e
Paweł Pacana <pawel.pacana@gmail.com>
2021-09-18 01:39:26 +0200

    Doh

--- rails_application/test/customer_repository_test.rb
+++ rails_application/test/customer_repository_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require_relative "../../ecommerce/crm/lib/crm/customer_repository_examples"
 
 class CustomerRepositoryTest < ActiveSupport::TestCase
-  include Crm::CustomerRepositoryExamples.for(-> { InMemoryCustomerRepository.new })
+  include Crm::CustomerRepositoryExamples.for(-> { CustomerRepository.new })
 
   def setup
     super
