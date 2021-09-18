d32b59efeac754288a6e3adbcbe3ec2558722524
Paweł Pacana <pawel.pacana@gmail.com>
2021-09-18 03:22:48 +0200

    Prefer stdlib in context

--- ecommerce/ordering/lib/ordering/number_generator.rb
+++ ecommerce/ordering/lib/ordering/number_generator.rb
@@ -1,7 +1,7 @@
 module Ordering
   class NumberGenerator
     def call
-      Time.current.strftime("%Y/%m/#{random_number}")
+      Time.now.strftime("%Y/%m/#{random_number}")
     end
 
     private
--- ecommerce/ordering/test/number_generator_test.rb
+++ ecommerce/ordering/test/number_generator_test.rb
@@ -3,11 +3,11 @@ require_relative "test_helper"
 module Ordering
   class NumberGeneratorTest < Test
     def test_includes_year
-      assert_includes(NumberGenerator.new.call, "#{Time.current.year}")
+      assert_includes(NumberGenerator.new.call, "#{Time.now.year}")
     end
 
     def test_includes_month
-      assert_includes(NumberGenerator.new.call, "#{Time.current.month}")
+      assert_includes(NumberGenerator.new.call, "#{Time.now.month}")
     end
   end
 end
