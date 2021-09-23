9e5de85f16387038550e68bbf247d23bb19ac7d5
Paweł Pacana <pawel.pacana@gmail.com>
2021-09-23 13:35:37 +0200

    Extract action buttons

--- rails_application/app/helpers/application_helper.rb
+++ rails_application/app/helpers/application_helper.rb
@@ -8,6 +8,41 @@ module ApplicationHelper
       )
   end
 
+  def action_button(css_classes, type: "button", form: nil)
+    content_tag "span", class: "sm:ml-3" do
+      content_tag "button", type: type, form: form, class: class_names(
+        "inline-flex items-center px-4 py-2 border rounded-md shadow-sm text-sm font-medium",
+        "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+        css_classes
+      ) do
+        yield
+      end
+    end
+  end
+
+  def primary_form_action_button(&block)
+    action_button(
+      "border-transparent text-white bg-blue-600 hover:bg-blue-700",
+      type: "submit",
+      form: "form",
+      &block
+    )
+  end
+
+  def primary_action_button(&block)
+    action_button(
+      "border-transparent text-white bg-blue-600 hover:bg-blue-700",
+      &block
+    )
+  end
+
+  def secondary_action_button(type: "button", form: nil, &block)
+    action_button(
+      "border-gray-300 text-gray-700 bg-white hover:bg-gray-50",
+      &block
+    )
+  end
+
   def stream_browser_path(stream_name)
     File.join(ruby_event_store_browser_app_path, "streams", stream_name)
   end
