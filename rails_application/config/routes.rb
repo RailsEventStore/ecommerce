688c799c659d6eb0d71f39159893c1ecd6131787
Manuel Ortega <maoc84@gmail.com>
2021-10-03 18:57:47 +0200

    Updates Rails app so customer is able to add the shipping address he or she wants to receive the shipment in

--- rails_application/config/routes.rb
+++ rails_application/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get :edit_discount
       post :update_discount
     end
+    resource :shipping_address, only: [:new, :create]
   end
   resources :products, only: [:new, :show, :create, :index] do
     resources :supplies, only: [:new, :create]
