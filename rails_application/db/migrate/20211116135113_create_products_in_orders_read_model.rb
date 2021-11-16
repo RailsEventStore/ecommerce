debf1efe51625fa6069298f2a77df37b8f9887ef
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2021-11-16 16:20:37 +0100

    Products lookup table - migration

---
+++ rails_application/db/migrate/20211116135113_create_products_in_orders_read_model.rb
@@ -0,0 +1,9 @@
+class CreateProductsInOrdersReadModel < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders_products do |t|
+      t.uuid    "uid", null: false
+      t.string  "name"
+      t.decimal "price", precision: 8, scale: 2
+    end
+  end
+end
