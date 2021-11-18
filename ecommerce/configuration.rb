cac8fa16698e04dfb93ee051919e728a16ff1ac7
Piotr Jurewicz <pjurewicz93@gmail.com>
2021-11-18 13:25:48 +0100

    Available VAT rates can be configured in Invoicing::Configuration

--- ecommerce/configuration.rb
+++ ecommerce/configuration.rb
@@ -5,35 +5,36 @@ require_relative "crm/lib/crm"
 require_relative "payments/lib/payments"
 require_relative "inventory/lib/inventory"
 require_relative "shipping/lib/shipping"
-require_relative "invoicing//lib/invoicing"
+require_relative "invoicing/lib/invoicing"
 require_relative "processes/lib/processes"
 
 module Ecommerce
   class Configuration
-    def initialize(number_generator: nil, payment_gateway: nil)
+    def initialize(number_generator: nil, payment_gateway: nil, available_vat_rates: [])
       @number_generator = number_generator
       @payment_gateway = payment_gateway
+      @available_vat_rates = available_vat_rates
     end
 
     def call(cqrs)
-      configure_bounded_contexts(cqrs, @number_generator, @payment_gateway)
+      configure_bounded_contexts(cqrs)
       configure_processes(cqrs)
     end
 
-    def configure_bounded_contexts(cqrs, number_generator, payment_gateway)
+    def configure_bounded_contexts(cqrs)
       raise ArgumentError.new(
         "Neither number_generator nor payment_gateway can be null"
-      ) if number_generator.nil? || payment_gateway.nil?
+      ) if @number_generator.nil? || @payment_gateway.nil?
       [
         Shipments::Configuration.new,
-        Ordering::Configuration.new(number_generator),
+        Ordering::Configuration.new(@number_generator),
         Pricing::Configuration.new,
-        Payments::Configuration.new(payment_gateway),
+        Payments::Configuration.new(@payment_gateway),
         ProductCatalog::Configuration.new,
         Crm::Configuration.new,
         Inventory::Configuration.new,
         Shipping::Configuration.new,
-        Invoicing::Configuration.new
+        Invoicing::Configuration.new(@available_vat_rates)
       ].each { |c| c.call(cqrs) }
     end
 
--- ecommerce/invoicing/lib/invoicing.rb
+++ ecommerce/invoicing/lib/invoicing.rb
@@ -2,7 +2,16 @@ require "infra"
 
 module Invoicing
   class Configuration
+    def self.AVAILABLE_VAT_RATES
+      @@available_vat_rates
+    end
+
+    def initialize available_vat_rates = []
+      @available_vat_rates = available_vat_rates
+    end
+
     def call(cqrs)
+      @@available_vat_rates = @available_vat_rates
       cqrs.register_command(GenerateInvoice, GenerateInvoiceHandler.new, InvoiceGenerated)
     end
   end
