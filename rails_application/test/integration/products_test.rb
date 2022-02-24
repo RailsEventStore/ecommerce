a10b049e18586166a33157f68da9a2388a7f0831
climbus <climbus@gmail.com>
2022-02-24 12:28:56 +0100

    test for creating product with unavailable vat rate

---
+++ rails_application/test/integration/products_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class ProductsTest < InMemoryRESIntegrationTestCase
+  cover "Products*"
+
+  def setup
+    Taxes::Configuration.available_vat_rates
+  end
+
+  def test_add_product_with_worng_vat_rate_code
+    product_id = SecureRandom.uuid
+    post "/products",
+         params: {
+           "authenticity_token" => "[FILTERED]",
+           "product_id" => SecureRandom.uuid,
+           "name" => "Product 1",
+           "vat_rate" => "50",
+         }
+    follow_redirect!
+    assert_select("#notice", "Selected VAT rate not applicable")
+  end
+end
