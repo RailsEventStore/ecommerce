848ac43baef06fea85b32e5040a32379547204d0
lukaszreszke <lukaszreszke93@gmail.com>
2022-03-04 20:14:01 +0100

    #94 refactored client orders tests. Changed all commands (except CancelOrder) to http calls to simulate user behavior better. CancelOrder is not yet changed because there's no endpoint for cancelling an order.

--- rails_application/test/integration/client_orders_test.rb
+++ rails_application/test/integration/client_orders_test.rb
@@ -8,26 +8,13 @@ class ClientOrdersTests < InMemoryRESIntegrationTestCase
     Rails.configuration.payment_gateway.call.reset
     ClientOrders::Client.destroy_all
     ClientOrders::Order.destroy_all
+    Orders::Order.destroy_all
   end
 
   def test_happy_path
-    shopify_id = SecureRandom.uuid
-    arkency_id = SecureRandom.uuid
-    run_command(
-      Crm::RegisterCustomer.new(customer_id: shopify_id, name: "Shopify")
-    )
-    run_command(
-      Crm::RegisterCustomer.new(customer_id: arkency_id, name: "Arkency")
-    )
-
-    async_remote_id = SecureRandom.uuid
-    run_command(
-      ProductCatalog::RegisterProduct.new(
-        product_id: async_remote_id,
-        name: "Async Remote"
-      )
-    )
-    run_command(Pricing::SetPrice.new(product_id: async_remote_id, price: 39))
+    register_customer('Shopify')
+    arkency_id = register_customer('Arkency')
+    async_remote_id = register_product("Async Remote", 39, 10)
 
     get "/client"
 
@@ -35,25 +22,22 @@ class ClientOrdersTests < InMemoryRESIntegrationTestCase
     assert_select("select", "Shopify\nArkency")
 
     login(arkency_id)
-
     assert_select("p", "No orders to display.")
 
     order_id = SecureRandom.uuid
-    run_command(Ordering::AddItemToBasket.new(product_id: async_remote_id, order_id: order_id))
-    run_command(Ordering::AddItemToBasket.new(product_id: async_remote_id, order_id: order_id))
-    run_command(Ordering::SubmitOrder.new(order_id: order_id, customer_id: arkency_id))
+    add_item_to_basket_for_order(async_remote_id, order_id)
+    submit_order_for_customer(arkency_id, order_id)
 
     login(arkency_id)
-
     assert_select("td", "Submitted")
 
-    pay(order_id)
+    pay_order(order_id)
 
     login(arkency_id)
 
     assert_select("td", "Paid")
 
-    cancel_submitted_order(arkency_id)
+    cancel_submitted_order_for_customer(arkency_id)
 
     login(arkency_id)
 
@@ -62,25 +46,41 @@ class ClientOrdersTests < InMemoryRESIntegrationTestCase
 
   private
 
-  def cancel_submitted_order(arkency_id)
-    order_id = SecureRandom.uuid
-    anti_if = SecureRandom.uuid
-    run_command(
-      ProductCatalog::RegisterProduct.new(
-        product_id: anti_if,
-        name: "Anti If"
-      )
-    )
-    run_command(Pricing::SetPrice.new(product_id: anti_if, price: 99))
-    run_command(Ordering::AddItemToBasket.new(product_id: anti_if, order_id: order_id))
-    run_command(Ordering::AddItemToBasket.new(product_id: anti_if, order_id: order_id))
-    run_command(Ordering::SubmitOrder.new(order_id: order_id, customer_id: arkency_id))
-    run_command(Ordering::CancelOrder.new(order_id: order_id))
+  def register_product(name, price, vat_rate)
+    async_remote_id = SecureRandom.uuid
+    post "/products", params: { product_id: async_remote_id, name: name, price: price, vat_rate: vat_rate }
+    async_remote_id
   end
 
-  def pay(order_id)
-    run_command(Payments::AuthorizePayment.new(order_id: order_id))
-    run_command(Payments::CapturePayment.new(order_id: order_id))
+  def register_customer(name)
+    customer_id = SecureRandom.uuid
+    post "/customers", params: { customer_id: customer_id, name: name }
+    customer_id
+  end
+
+  def submit_order_for_customer(customer_id, order_id)
+    post "/orders", params: { order_id: order_id, customer_id: customer_id }
+    follow_redirect!
+  end
+
+  def add_item_to_basket_for_order(async_remote_id, order_id)
+    post "/orders/#{order_id}/add_item?product_id=#{async_remote_id}"
+    follow_redirect!
+  end
+
+  def pay_order(order_id)
+    post "/orders/#{order_id}/pay"
+  end
+
+  def cancel_submitted_order_for_customer(customer_id)
+    order_id = SecureRandom.uuid
+    anti_if = register_product('Anti If', 99, 10)
+
+    add_item_to_basket_for_order(anti_if, order_id)
+    add_item_to_basket_for_order(anti_if, order_id)
+    submit_order_for_customer(customer_id, order_id)
+
+    run_command(Ordering::CancelOrder.new(order_id: order_id))
   end
 
   def login(arkency_id)
