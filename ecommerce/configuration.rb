11041c676314d67be4494bcb4e567de9b9e63966
Sebastian Wilgosz <sebastian@hanamimastery.com>
2022-03-13 21:53:05 +0100

    TMP: disable everything from ecomerce except CRM for easier integration and debugging

--- ecommerce/configuration.rb
+++ ecommerce/configuration.rb
@@ -1,13 +1,13 @@
-require_relative "ordering/lib/ordering"
-require_relative "pricing/lib/pricing"
-require_relative "product_catalog/lib/product_catalog"
+# require_relative "ordering/lib/ordering"
+# require_relative "pricing/lib/pricing"
+# require_relative "product_catalog/lib/product_catalog"
 require_relative "crm/lib/crm"
-require_relative "payments/lib/payments"
-require_relative "inventory/lib/inventory"
-require_relative "shipping/lib/shipping"
-require_relative "invoicing/lib/invoicing"
-require_relative "taxes/lib/taxes"
-require_relative "processes/lib/processes"
+# require_relative "payments/lib/payments"
+# require_relative "inventory/lib/inventory"
+# require_relative "shipping/lib/shipping"
+# require_relative "invoicing/lib/invoicing"
+# require_relative "taxes/lib/taxes"
+# require_relative "processes/lib/processes"
 
 module Ecommerce
   class Configuration
@@ -19,7 +19,7 @@ module Ecommerce
 
     def call(cqrs)
       configure_bounded_contexts(cqrs)
-      configure_processes(cqrs)
+      # configure_processes(cqrs)
     end
 
     def configure_bounded_contexts(cqrs)
@@ -27,21 +27,21 @@ module Ecommerce
         "Neither number_generator nor payment_gateway can be null"
       ) if @number_generator.nil? || @payment_gateway.nil?
       [
-        Shipments::Configuration.new,
-        Ordering::Configuration.new(@number_generator),
-        Pricing::Configuration.new,
-        Payments::Configuration.new(@payment_gateway),
-        ProductCatalog::Configuration.new,
+        # Shipments::Configuration.new,
+        # Ordering::Configuration.new(@number_generator),
+        # Pricing::Configuration.new,
+        # Payments::Configuration.new(@payment_gateway),
+        # ProductCatalog::Configuration.new,
         Crm::Configuration.new,
-        Inventory::Configuration.new,
-        Shipping::Configuration.new,
-        Invoicing::Configuration.new,
-        Taxes::Configuration.new(@available_vat_rates)
+        # Inventory::Configuration.new,
+        # Shipping::Configuration.new,
+        # Invoicing::Configuration.new,
+        # Taxes::Configuration.new(@available_vat_rates)
       ].each { |c| c.call(cqrs) }
     end
 
-    def configure_processes(cqrs)
-      Processes::Configuration.new.call(cqrs)
-    end
+    # def configure_processes(cqrs)
+    #   Processes::Configuration.new.call(cqrs)
+    # end
   end
 end
