cf89d8aa56bd141a98199a7580e5240fa011c8b8
krzykamil <krzysiekkamilpiotrek@gmail.com>
2022-05-25 15:23:18 +0200

    adjust rails app usage of coupons

--- rails_application/app/read_models/coupons/configuration.rb
+++ rails_application/app/read_models/coupons/configuration.rb
@@ -7,7 +7,7 @@ module Coupons
     def call(cqrs)
       cqrs.subscribe(
         -> (event) { register_coupon(event) },
-        [Pricing::Coupon::CouponRegistered]
+        [Pricing::CouponRegistered]
       )
     end
 
