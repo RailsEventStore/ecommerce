c2f5755257960957b40f7c5c5163eb3affb681e2
Brychu <pbrych@gmail.com>
2022-05-29 00:37:35 +0200

    Add read model configuration

---
+++ rails_application/app/read_models/happy_hours/configuration.rb
@@ -0,0 +1,21 @@
+module HappyHours
+  class HappyHour < ApplicationRecord
+    self.table_name = "happy_hours"
+  end
+
+  class Configuration
+    def call(cqrs)
+      cqrs.subscribe(
+        ->(event) { create_happy_hour(event) },
+        [Pricing::HappyHourCreated]
+      )
+    end
+
+    private
+
+    def create_happy_hour(event)
+      event_data = event.data
+      HappyHour.create(**event_data.merge(uid: event_data.fetch(:happy_hour_id)))
+    end
+  end
+end
