7cce9c358ff2030c6381ea2fe3ff1a7ec3cae621
Brychu <pbrych@gmail.com>
2022-05-28 15:27:47 +0200

    Add create_happy_hour method to the Pricing::Product aggregate

---
+++ ecommerce/pricing/test/product_test.rb
@@ -0,0 +1,57 @@
+require_relative "test_helper"
+
+module Pricing
+  class ProductTest < Test
+    cover "Pricing::Product*"
+
+    def test_product_price_is_set
+      product_id = SecureRandom.uuid
+
+      set_price(product_id, 20)
+    end
+
+    def test_product_happy_hour_is_created
+      product_id = SecureRandom.uuid
+      discount_in_percent = 30
+      start_hour = 15
+      end_hour = 18
+
+      create_happy_hour(product_id, discount_in_percent, start_hour, end_hour)
+    end
+
+    def test_can_add_more_happy_hours
+      product_id = SecureRandom.uuid
+
+      create_happy_hour(product_id, 50, 12, 15)
+      create_happy_hour(product_id, 30, 15, 18)
+      create_happy_hour(product_id, 10, 18, 20)
+    end
+
+    def test_cannot_add_overlapping_happy_hours
+      product_id = SecureRandom.uuid
+
+      create_happy_hour(product_id, 50, 12, 15)
+
+      assert_raises Pricing::Product::OverlappingHappyHour do
+        create_happy_hour(product_id, 50, 13, 16)
+      end
+    end
+
+    private
+
+    def set_price(product_id, amount)
+      run_command(SetPrice.new(product_id: product_id, price: amount))
+    end
+
+    def create_happy_hour(product_id, discount, start_hour, end_hour)
+      run_command(
+        CreateHappyHour.new(
+          product_id: product_id,
+          discount: discount,
+          start_hour: start_hour,
+          end_hour: end_hour
+        )
+      )
+    end
+  end
+end
