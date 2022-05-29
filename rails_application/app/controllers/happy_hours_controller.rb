c5e7363288a61bed31ef7eb8ddd6c80c6efc3cd5
Brychu <pbrych@gmail.com>
2022-05-29 17:54:15 +0200

    Add basic UI for creating happy hours

--- rails_application/app/controllers/happy_hours_controller.rb
+++ rails_application/app/controllers/happy_hours_controller.rb
@@ -5,12 +5,19 @@ class HappyHoursController < ApplicationController
 
   def new
     @happy_hour_id = SecureRandom.uuid
+    @products = Products::Product.all
   end
 
   def create
-    create_happy_hour(params.slice(:name, :code, :discount, :start_hour, :end_hour, :product_ids))
+    create_happy_hour(
+      **params.permit(
+        :happy_hour_id, :name, :code, :discount, :start_hour, :end_hour, product_ids: []
+      )
+    )
   rescue Pricing::HappyHour::AlreadyCreated
-    flash[:notice] = "Happy hour is already created"
+    flash[:error] = "Happy hour is already created"
+    @happy_hour_id = SecureRandom.uuid
+    @products = Products::Product.all
     render "new"
   else
     redirect_to happy_hours_path, notice: "Happy hour was successfully created"
@@ -19,10 +26,15 @@ class HappyHoursController < ApplicationController
   private
 
   def create_happy_hour(**kwargs)
-    command_bus.(create_happy_hour_cmd(kwargs))
+    command_bus.(create_happy_hour_cmd(**kwargs))
   end
 
   def create_happy_hour_cmd(**kwargs)
-    Pricing::CreateHappyHour.new(kwargs)
+    Pricing::CreateHappyHour.new(**kwargs.symbolize_keys)
   end
+
+  def product_names_for(happy_hour)
+    Products::Product.where(id: happy_hour.product_ids).pluck(:name).join(", ")
+  end
+  helper_method :product_names_for
 end
