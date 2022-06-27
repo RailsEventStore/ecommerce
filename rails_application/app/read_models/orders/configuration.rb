4288fe2c3d967f7701904f10e00148e9f9fbcde0
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-06-27 08:54:23 +0200

    Revert "Show amount after applying happy hour on the UI"
    This reverts commit 743f9b00bb3f322e29694135692b16757e1cfd39.

--- rails_application/app/read_models/orders/configuration.rb
+++ rails_application/app/read_models/orders/configuration.rb
@@ -169,9 +169,7 @@ module Orders
 
     def update_totals(event)
       with_order(event) do |order|
-        order.happy_hour_value = event.data.fetch(:happy_hour_amount)
         order.discounted_value = event.data.fetch(:discounted_amount)
-        order.total_value = event.data.fetch(:total_amount)
       end
     end
 
