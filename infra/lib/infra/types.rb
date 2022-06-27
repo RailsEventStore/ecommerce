ff1a75cc7c7efdfe7e40728805beca6c622a57eb
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-06-27 08:54:23 +0200

    Revert "Use separate type for discount"
    This reverts commit 576f250e683928821224d2bed1c4b1ad17ecb592.

--- infra/lib/infra/types.rb
+++ infra/lib/infra/types.rb
@@ -17,7 +17,6 @@ module Infra
     Value = Types::Coercible::Decimal
     PercentageDiscount = Types::Coercible::Decimal.constrained(gt: 0, lteq: 100)
     CouponDiscount = Types::Coercible::Float.constrained(gt: 0, lteq: 100)
-    HappyHourDiscount = Types::Coercible::Integer.constrained(gt: 0, lteq: 100)
     UUIDQuantityHash = Types::Hash.map(UUID, Quantity)
     Hour = Types::Coercible::Integer.constrained(lt: 24)
 
