3c0eabf5897d391ed54db806c90e206e475287ec
Brychu <pbrych@gmail.com>
2022-06-30 00:03:28 +0200

    Add spec and other adjustments to satisfy mutant

--- ecommerce/pricing/lib/pricing/helpers/happy_hours_for_product.rb
+++ ecommerce/pricing/lib/pricing/helpers/happy_hours_for_product.rb
@@ -15,15 +15,14 @@ module Pricing
         combined_schedule = build_combined_schedule(product_ids)
         new_happy_hour = build_happy_hour(start_hour, end_hour)
 
-        combined_schedule.select { |hour, _| new_happy_hour.include?(hour) }.values.flatten.uniq
+        combined_schedule.select { |hour,| new_happy_hour.include?(hour) }.values.flatten.uniq
       end
 
       private
 
       def product_happy_hours(product_id)
-        events = @event_store.read.stream("Pricing::Product$#{product_id}").of_type(ProductAddedToHappyHour).to_a
-
-        product = Product.new(product_id)
+        events = @event_store.read.stream("Pricing::Product$#{product_id}")
+        product = Product.new
 
         product.apply(*events)
 
@@ -39,7 +38,7 @@ module Pricing
 
           schedule.keys.each do |hour|
             combined_schedule[hour] ||= []
-            combined_schedule[hour] << product_id
+            combined_schedule.fetch(hour) << product_id
           end
         end
 
@@ -51,10 +50,20 @@ module Pricing
           first_segment = Range.new(0, end_hour - 1).to_a
           second_segment = Range.new(start_hour, 23).to_a
 
+          validate_hours_list(first_segment)
+          validate_hours_list(second_segment)
+
           first_segment.union(second_segment)
         else
-          Range.new(start_hour, end_hour - 1).to_a
+          Range.new(start_hour, end_hour - 1)
         end
+      end
+
+      def validate_hours_list(sorted_hours)
+        return if sorted_hours.empty?
+
+        Infra::Types::Hour[sorted_hours.first]
+        Infra::Types::Hour[sorted_hours.last]
       end
     end
   end
--- ecommerce/pricing/test/helpers/happy_hours_for_product_test.rb
+++ ecommerce/pricing/test/helpers/happy_hours_for_product_test.rb
@@ -5,6 +5,10 @@ module Pricing
     class HappyHoursForProductTest < Test
       cover "Pricing::Helpers::HappyHoursForProduct*"
 
+      def setup
+        @helper = Pricing::Helpers::HappyHoursForProduct.new(cqrs.event_store)
+      end
+
       def test_returns_discount_for_specific_hour
         some_product_id = SecureRandom.uuid
         run_command(
@@ -34,11 +38,103 @@ module Pricing
           )
         )
 
-        helper_instance = Pricing::Helpers::HappyHoursForProduct.new(cqrs.event_store)
+        assert @helper.discount_for(some_product_id, 21) == 25
+        assert @helper.discount_for(some_product_id, 19).nil?
+        assert @helper.discount_for(some_product_id, 17) == 15
+      end
 
-        assert helper_instance.discount_for(some_product_id, 21) == 25
-        assert helper_instance.discount_for(some_product_id, 19).nil?
-        assert helper_instance.discount_for(some_product_id, 17) == 15
+      def test_returns_products_with_overlapping_happy_hours
+        first_product_id = SecureRandom.uuid
+        second_product_id = SecureRandom.uuid
+        third_product_id = SecureRandom.uuid
+        fourth_product_id = SecureRandom.uuid
+        fifth_product_id = SecureRandom.uuid
+
+        run_command(
+          Pricing::CreateHappyHour.new(
+            details: {
+              id: SecureRandom.uuid,
+              name: "Night Owls",
+              code: "owls",
+              discount: "25",
+              start_hour: "20",
+              end_hour: "2",
+              product_ids: [first_product_id, third_product_id]
+            }
+          )
+        )
+        run_command(
+          Pricing::CreateHappyHour.new(
+            details: {
+              id: SecureRandom.uuid,
+              name: "Tea Time",
+              code: "tea",
+              discount: "15",
+              start_hour: "17",
+              end_hour: "19",
+              product_ids: [second_product_id]
+            }
+          )
+        )
+        run_command(
+          Pricing::CreateHappyHour.new(
+            details: {
+              id: SecureRandom.uuid,
+              name: "High Noon",
+              code: "high_noon",
+              discount: "50",
+              start_hour: "12",
+              end_hour: "13",
+              product_ids: [fourth_product_id]
+            }
+          )
+        )
+        run_command(
+          Pricing::CreateHappyHour.new(
+            details: {
+              id: SecureRandom.uuid,
+              name: "Drrracula",
+              code: "dracula",
+              discount: "33",
+              start_hour: "0",
+              end_hour: "1",
+              product_ids: [fifth_product_id]
+            }
+          )
+        )
+
+        products = [first_product_id, second_product_id, third_product_id, fourth_product_id, fifth_product_id]
+
+        result = @helper.products_with_overlapping_happy_hours(products, 18, 22).sort
+        assert result == [first_product_id, second_product_id, third_product_id].sort
+
+        result = @helper.products_with_overlapping_happy_hours(products, 23, 5).sort
+        assert result == [first_product_id, third_product_id, fifth_product_id].sort
+
+        result = @helper.products_with_overlapping_happy_hours(products, 23, 0).sort
+        assert result == [first_product_id, third_product_id].sort
+
+        result = @helper.products_with_overlapping_happy_hours(products, 23, 1).sort
+        assert result == [first_product_id, third_product_id, fifth_product_id].sort
+
+        result = @helper.products_with_overlapping_happy_hours(products, 19, 20)
+        assert result == []
+
+        result = @helper.products_with_overlapping_happy_hours(products, 17, 18)
+        assert result == [second_product_id]
+
+        result = @helper.products_with_overlapping_happy_hours(products, 17, 17).sort
+        assert result == products.sort
+
+        result = -> { @helper.products_with_overlapping_happy_hours(products, 36, 35) }
+        assert_raises(Dry::Types::ConstraintError) do
+          result.call
+        end
+
+        result = -> { @helper.products_with_overlapping_happy_hours(products, -2, -2) }
+        assert_raises(Dry::Types::ConstraintError) do
+          result.call
+        end
       end
 
       private
@@ -48,8 +144,6 @@ module Pricing
           CreateHappyHour.new(kwargs)
         )
       end
-
-
     end
   end
 end
