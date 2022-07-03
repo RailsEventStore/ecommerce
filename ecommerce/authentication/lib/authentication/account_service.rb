c28ab36931b8b9b50054eb879e19e87c4ba12734
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2022-07-03 23:55:12 +0200

    Add Authentication::SetPasswordHash command

--- ecommerce/authentication/lib/authentication/account_service.rb
+++ ecommerce/authentication/lib/authentication/account_service.rb
@@ -22,4 +22,16 @@ module Authentication
       end
     end
   end
+
+  class OnPasswordHashSet
+    def initialize(event_store)
+      @repository = Infra::AggregateRootRepository.new(event_store)
+    end
+
+    def call(command)
+      @repository.with_aggregate(Account, command.aggregate_id) do |account|
+        account.set_password_hash(command.password_hash)
+      end
+    end
+  end
 end
