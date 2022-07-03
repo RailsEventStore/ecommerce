95fdee147de585ff917b99ab12d0cb78c6cacf36
Brychu <pbrych@gmail.com>
2022-07-03 14:32:34 +0200

    Drop happy hours and create migrations table

---
+++ rails_application/db/migrate/20220703122711_drop_happy_hours.rb
@@ -0,0 +1,14 @@
+class DropHappyHours < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :happy_hours, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+      t.string :name
+      t.string :code
+      t.integer :discount
+      t.integer :start_hour
+      t.integer :end_hour
+      t.string :product_ids, array: true
+
+      t.timestamps
+    end
+  end
+end
