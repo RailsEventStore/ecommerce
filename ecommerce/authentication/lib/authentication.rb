a625de40e80e48b9568e752d8d5691c0f5fd9e15
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2022-07-03 23:55:12 +0200

    Add Authentication::SetPasswordHash command

--- ecommerce/authentication/lib/authentication.rb
+++ ecommerce/authentication/lib/authentication.rb
@@ -1,8 +1,10 @@
 require "infra"
 require_relative "authentication/commands/set_login"
+require_relative "authentication/commands/set_password_hash"
 require_relative "authentication/commands/register_account"
 require_relative "authentication/events/account_registered"
 require_relative "authentication/events/login_set"
+require_relative "authentication/events/password_hash_set"
 require_relative "authentication/account_service"
 require_relative "authentication/account"
 
@@ -11,6 +13,7 @@ module Authentication
     def call(cqrs)
       cqrs.register_command(RegisterAccount, OnRegistration.new(cqrs.event_store), AccountRegistered)
       cqrs.register_command(SetLogin, OnLoginSet.new(cqrs.event_store), LoginSet)
+      cqrs.register_command(SetPasswordHash, OnPasswordHashSet.new(cqrs.event_store), PasswordHashSet)
     end
   end
 end
--- ecommerce/authentication/lib/authentication/account.rb
+++ ecommerce/authentication/lib/authentication/account.rb
@@ -18,12 +18,20 @@ module Authentication
       apply LoginSet.new(data: { account_id: @id, login: login })
     end
 
+    def set_password_hash(password_hash)
+      apply PasswordHashSet.new(data: { account_id: @id, password_hash: password_hash })
+    end
+
     on AccountRegistered do |event|
       @registered = true
     end
 
     on LoginSet do |event|
       @login = event.data[:login]
+    end
+
+    on PasswordHashSet do |event|
+      @password_hash = event.data[:password_hash]
     end
   end
 end
