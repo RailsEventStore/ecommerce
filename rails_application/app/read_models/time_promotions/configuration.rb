d1ba3ea9ae607c59f5b74c9d14f9521de9d0fa91
Brychu <pbrych@gmail.com>
2022-07-06 00:09:55 +0200

    Add scope for read model

--- rails_application/app/read_models/time_promotions/configuration.rb
+++ rails_application/app/read_models/time_promotions/configuration.rb
@@ -1,6 +1,8 @@
 module TimePromotions
   class TimePromotion < ApplicationRecord
     self.table_name = "time_promotions"
+
+    scope :current, -> { where("start_time < ? AND end_time > ?", DateTime.now, DateTime.now) }
   end
 
   class Configuration
