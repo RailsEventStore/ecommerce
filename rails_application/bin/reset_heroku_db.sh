4b9d4964011b228f56ced915c291ac6ad5d10108
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-07-12 10:38:31 +0200

    Fixed reset_db links
    There was a change on how we use heroku (buildpack) which sounds related to this. The current directory is different.

--- rails_application/bin/reset_heroku_db.sh
+++ rails_application/bin/reset_heroku_db.sh
@@ -2,5 +2,5 @@
 
 heroku restart --app=res-ecommerce-rails
 heroku pg:reset DATABASE --app=res-ecommerce-rails --confirm res-ecommerce-rails
-heroku run rake db:migrate --app=res-ecommerce-rails
-heroku run rake db:seed --app=res-ecommerce-rails
+heroku run "cd rails_application; rake db:migrate" --app=res-ecommerce-rails
+heroku run "cd rails_application; rake db:seed" --app=res-ecommerce-rails
