4a275c236f1f6eb5df20bbf11c1ccc5c26e65155
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-08-03 15:24:21 +0200

    removed Client Panel show order details
    It was implemented without tests and with coupling to another read model
    Later we will do it better. Coupling is acceptable, the lack of tests makes it more problematic.

--- rails_application/app/controllers/client/orders_controller.rb
+++ rails_application/app/controllers/client/orders_controller.rb
@@ -7,10 +7,5 @@ module Client
       render html: ClientOrders::OrdersList.build(view_context, cookies[:client_id]), layout: true
     end
 
-
-    def show
-      @order = ClientOrders::Order.find_by(order_uid: params[:order_uid], client_uid: params[:client_id]).order
-      @order_lines = Orders::OrderLine.where(order_uid: @order.uid)
-    end
   end
 end
