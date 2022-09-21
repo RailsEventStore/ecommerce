14539f83433d89effb2faa1a620c02ebaffe066c
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-09-21 16:33:12 +0200

    More declarative process API for SyncPricingFromOrdering

--- ecommerce/processes/lib/processes/sync_pricing_from_ordering.rb
+++ ecommerce/processes/lib/processes/sync_pricing_from_ordering.rb
@@ -1,65 +1,22 @@
 module Processes
   class SyncPricingFromOrdering
     def initialize(cqrs)
-      sync_basket_with_pricing(cqrs)
-      calculate_total_value_when_order_submitted(cqrs)
-      calculate_sub_amounts_when_order_submitted(cqrs)
-    end
-
-    private
-
-    def sync_basket_with_pricing(cqrs)
-      cqrs.subscribe(
-        ->(event) do
-          cqrs.run(
-            Pricing::AddPriceItem.new(
-              order_id: event.data.fetch(:order_id),
-              product_id: event.data.fetch(:product_id)
-            )
-          )
-        end,
-        [Ordering::ItemAddedToBasket]
+      cqrs.process(
+        Ordering::ItemAddedToBasket, [:order_id, :product_id],
+        Pricing::AddPriceItem,       [:order_id, :product_id]
       )
-
-      cqrs.subscribe(
-        ->(event) do
-          cqrs.run(
-            Pricing::RemovePriceItem.new(
-              order_id: event.data.fetch(:order_id),
-              product_id: event.data.fetch(:product_id)
-            )
-          )
-        end,
-        [Ordering::ItemRemovedFromBasket]
+      cqrs.process(
+        Ordering::ItemRemovedFromBasket, [:order_id, :product_id],
+        Pricing::RemovePriceItem,        [:order_id, :product_id]
+      )
+      cqrs.process(
+        Ordering::OrderSubmitted,     [:order_id],
+        Pricing::CalculateTotalValue, [:order_id]
+      )
+      cqrs.process(
+        Ordering::OrderSubmitted,     [:order_id],
+        Pricing::CalculateSubAmounts, [:order_id]
       )
     end
-
-
-    def calculate_total_value_when_order_submitted(cqrs)
-      cqrs.subscribe(
-        ->(event) do
-          cqrs.run(
-            Pricing::CalculateTotalValue.new(
-              order_id: event.data.fetch(:order_id)
-            )
-          )
-        end,
-        [Ordering::OrderSubmitted]
-      )
-    end
-
-    def calculate_sub_amounts_when_order_submitted(cqrs)
-      cqrs.subscribe(
-        ->(event) do
-          cqrs.run(
-            Pricing::CalculateSubAmounts.new(
-              order_id: event.data.fetch(:order_id)
-            )
-          )
-        end,
-        [Ordering::OrderSubmitted]
-      )
-    end
-
   end
 end
