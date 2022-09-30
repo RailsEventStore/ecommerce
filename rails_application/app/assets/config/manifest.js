fd7b3cd638df43c3bda3ae7976741b507646f462
pjurewicz <pjurewicz93@gmail.com>
2022-09-30 13:59:47 +0200

    tailwindcss bumped

--- rails_application/app/assets/config/manifest.js
+++ rails_application/app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link application.js
 //= link_directory ../stylesheets .css
+//= link_tree ../builds
---
+++ rails_application/config/tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
