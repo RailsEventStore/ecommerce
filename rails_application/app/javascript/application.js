8265faf97a1786440a5d8b4e8312a8cf9fcd600b
pjurewicz <pjurewicz93@gmail.com>
2022-10-03 17:54:30 +0200

    ActionCable enabled

--- rails_application/app/javascript/application.js
+++ rails_application/app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Rails from '@rails/ujs';
 import { Turbo } from "@hotwired/turbo-rails"
+import "channels"
 
 Rails.start();
--- rails_application/config/importmap.rb
+++ rails_application/config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
