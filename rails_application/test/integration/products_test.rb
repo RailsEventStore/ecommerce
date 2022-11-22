e6e3ccb284f9a4dd7f428027edcad54daa5d6da4
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2022-11-22 12:42:41 +0100

    assert price changed

--- rails_application/test/integration/products_test.rb
+++ rails_application/test/integration/products_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ProductsTest < InMemoryRESIntegrationTestCase
+  include ActionView::Helpers::NumberHelper
   cover "Products*"
 
   def setup
@@ -22,6 +23,9 @@ class ProductsTest < InMemoryRESIntegrationTestCase
            "vat_rate" => "10"
          }
     follow_redirect!
+
+    assert_equal "20.01", number_to_currency(Products::Product.find(product_id).price, unit: "")
+
     assert_select "h1", "Products"
 
     get "/products/#{product_id}/edit"
@@ -31,5 +35,7 @@ class ProductsTest < InMemoryRESIntegrationTestCase
            "product_id" => product_id,
            price: "20.02",
          }
+
+    assert_equal "20.02", number_to_currency(Products::Product.find(product_id).price, unit: "")
   end
 end
