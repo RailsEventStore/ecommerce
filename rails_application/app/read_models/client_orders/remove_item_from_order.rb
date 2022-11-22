8a4bf2d580170fe70b67f25a8fb896265d12b31a
lukaszreszke <lukaszreszke93@gmail.com>
2022-11-22 12:17:24 +0100

    Turbo broadcast when item removed

--- rails_application/app/read_models/client_orders/remove_item_from_order.rb
+++ rails_application/app/read_models/client_orders/remove_item_from_order.rb
@@ -1,9 +1,21 @@
 module ClientOrders
   class RemoveItemFromOrder < Infra::EventHandler
     def call(event)
-      item = find(event.data.fetch(:order_id), event.data.fetch(:product_id))
+      product_id = event.data.fetch(:product_id)
+      order_id = event.data.fetch(:order_id)
+      item = find(order_id , product_id)
       item.product_quantity -= 1
       item.product_quantity > 0 ? item.save! : item.destroy!
+
+      Turbo::StreamsChannel.broadcast_update_to(
+        "client_orders_#{order_id}",
+        target: "client_orders_#{product_id}_product_quantity",
+        html: item.product_quantity)
+
+      Turbo::StreamsChannel.broadcast_update_to(
+        "client_orders_#{order_id}",
+        target: "client_orders_#{product_id}_value",
+        html: ActiveSupport::NumberHelper.number_to_currency(item.value))
     end
 
     private
