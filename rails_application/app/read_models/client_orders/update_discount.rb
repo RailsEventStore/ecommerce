d8d37dab2020527f26d78aaabb1e7681b8e57e0c
lukaszreszke <lukaszreszke93@gmail.com>
2022-11-22 10:33:45 +0100

    Prefer fail&retry over silent swallowing of an event

--- rails_application/app/read_models/client_orders/update_discount.rb
+++ rails_application/app/read_models/client_orders/update_discount.rb
@@ -2,10 +2,8 @@ module ClientOrders
   class UpdateDiscount < Infra::EventHandler
     def call(event)
       order = Order.find_by(order_uid: event.data.fetch(:order_id))
-      unless order.nil?
-        order.percentage_discount = event.data.fetch(:amount)
-        order.save!
-      end
+      order.percentage_discount = event.data.fetch(:amount)
+      order.save!
     end
   end
 end
