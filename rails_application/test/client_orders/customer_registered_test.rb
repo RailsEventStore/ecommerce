1b39e6aac1d6cf0d78b1044625d823ccbf7487e6
lukaszreszke <lukaszreszke93@gmail.com>
2022-11-22 10:33:15 +0100

    Create an order if it doesn't exist
    kills mutants

--- rails_application/test/client_orders/customer_registered_test.rb
+++ rails_application/test/client_orders/customer_registered_test.rb
@@ -27,5 +27,29 @@ module ClientOrders
       assert_equal(customer_id, client.uid)
     end
 
+    def test_customer_assigned_to_order
+      event_store = Rails.configuration.event_store
+
+      customer_id = SecureRandom.uuid
+      order_id = SecureRandom.uuid
+
+      event_store.publish(Crm::CustomerRegistered.new(
+        data: {
+          customer_id: customer_id,
+          name: "John Doe"
+        }
+      ))
+
+      event_store.publish(Crm::CustomerAssignedToOrder.new(
+        data: {
+          customer_id: customer_id,
+          order_id: order_id
+        }
+      ))
+
+      order = Order.find_by(order_uid: order_id)
+      assert_equal "Draft", order.state
+    end
+
   end
 end
