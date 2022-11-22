d06b349faf47dfaeac5642f7a9b505844364f5e4
lukaszreszke <lukaszreszke93@gmail.com>
2022-11-22 10:33:15 +0100

    Create an order if it doesn't exist
    kills mutants

--- rails_application/app/read_models/client_orders/assign_customer_to_order.rb
+++ rails_application/app/read_models/client_orders/assign_customer_to_order.rb
@@ -1,11 +1,15 @@
 module ClientOrders
   class AssignCustomerToOrder < Infra::EventHandler
     def call(event)
-      order = Order.find_by(order_uid: event.data.fetch(:order_id))
-      unless order.nil?
-        order.client_uid = event.data.fetch(:customer_id)
-        order.save!
+      order_uid = event.data.fetch(:order_id)
+      order = Order.find_by(order_uid: order_uid)
+
+      if order.nil?
+        order = Order.create!(order_uid: order_uid, state: "Draft")
       end
+
+      order.client_uid = event.data.fetch(:customer_id)
+      order.save!
     end
   end
 end
