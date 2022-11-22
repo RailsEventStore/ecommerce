f1ced7ff3835e6ce1ad8fc93f71def9d6eb1e738
lukaszreszke <lukaszreszke93@gmail.com>
2022-11-22 10:33:45 +0100

    Prefer fail&retry over silent swallowing of an event

--- rails_application/app/read_models/client_orders/cancel_order.rb
+++ rails_application/app/read_models/client_orders/cancel_order.rb
@@ -2,10 +2,8 @@ module ClientOrders
   class CancelOrder < Infra::EventHandler
     def call(event)
       order = Order.find_by(order_uid: event.data.fetch(:order_id))
-      unless order.nil?
-        order.state = "Cancelled"
-        order.save!
-      end
+      order.state = "Cancelled"
+      order.save!
     end
   end
 end
--- rails_application/app/read_models/client_orders/update_order_total_value.rb
+++ rails_application/app/read_models/client_orders/update_order_total_value.rb
@@ -2,11 +2,9 @@ module ClientOrders
   class UpdateOrderTotalValue < Infra::EventHandler
     def call(event)
       order = Order.find_by(order_uid: event.data.fetch(:order_id))
-      unless order.nil?
-        order.discounted_value = event.data.fetch(:discounted_amount)
-        order.total_value = event.data.fetch(:total_amount)
-        order.save!
-      end
+      order.discounted_value = event.data.fetch(:discounted_amount)
+      order.total_value = event.data.fetch(:total_amount)
+      order.save!
     end
   end
 end
