34226e839df1d81518209504204bf67e683fb39d
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-06 14:09:54 +0100

    Refactor
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>

--- rails_application/app/read_models/orders/broadcaster.rb
+++ rails_application/app/read_models/orders/broadcaster.rb
@@ -1,9 +1,9 @@
 module Orders
   class Broadcaster < Infra::EventHandler
-    def broadcast_update(order_id, product_id, target, content)
+    def broadcast_update(stream_id, target_id, target_name, content)
       Turbo::StreamsChannel.broadcast_update_to(
-        "orders_order_#{order_id}",
-        target: "orders_order_#{product_id}_#{target}",
+        "orders_order_#{stream_id}",
+        target: "orders_order_#{target_id}_#{target_name}",
         html: content)
     end
   end
