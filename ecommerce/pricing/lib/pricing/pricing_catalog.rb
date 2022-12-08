1cc4c949df6a92be448ea79618e98f0284d7155d
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2022-12-08 22:22:25 +0100

    method to rebuild future prices catalog

--- ecommerce/pricing/lib/pricing/pricing_catalog.rb
+++ ecommerce/pricing/lib/pricing/pricing_catalog.rb
@@ -14,18 +14,32 @@ module Pricing
     end
 
     def price_by_product_id(product_id)
-      @event_store
-        .read
-        .of_type(PriceSet)
-        .to_a
-        .filter { |e| e.data.fetch(:product_id).eql?(product_id) }
+      read_prices_set(product_id)
         .reject(&method(:reject_future_prices))
         .last
         .data
         .fetch(:price)
     end
 
+    def future_prices_catalog_by_product_id(product)
+      read_prices_set(product_id)
+        .select(&method(:only_future_prices))
+        .slice(:price, :valid_at)
+    end
+
     private
+
+    def read_prices_set(product_id)
+      @event_store
+        .read
+        .of_type(PriceSet)
+        .to_a
+        .filter { |e| e.data.fetch(:product_id).eql?(product_id) }
+    end
+
+    def only_future_prices(e)
+      !reject_future_prices(e)
+    end
 
     def reject_future_prices(e)
       e.metadata.fetch(:valid_at) > Time.now
