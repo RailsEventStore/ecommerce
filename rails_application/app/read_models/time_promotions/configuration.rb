ad27fc855a998301e278734a12b210a560d82434
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-09 13:06:08 +0100

    Remove unused methods

--- rails_application/app/read_models/time_promotions/configuration.rb
+++ rails_application/app/read_models/time_promotions/configuration.rb
@@ -12,31 +12,5 @@ module TimePromotions
       event_store.subscribe(SetDiscount, to: [Pricing::TimePromotionDiscountSet])
       event_store.subscribe(SetRange, to: [Pricing::TimePromotionRangeSet])
     end
-
-    private
-
-    def create_time_promotion(event)
-      id = event.data[:time_promotion_id]
-
-      TimePromotion.create!(event.data.slice(:code).merge(id: id))
-    end
-
-    def label(event)
-      id = event.data[:time_promotion_id]
-
-      TimePromotion.find_by(id: id).update!(label: event.data[:label])
-    end
-
-    def set_discount(event)
-      id = event.data[:time_promotion_id]
-
-      TimePromotion.find_by(id: id).update!(discount: event.data[:discount])
-    end
-
-    def set_range(event)
-      id = event.data[:time_promotion_id]
-
-      TimePromotion.find_by(id: id).update!(start_time: event.data[:start_time], end_time: event.data[:end_time])
-    end
   end
 end
