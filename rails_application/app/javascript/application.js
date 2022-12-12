e996370b1fd6cc9e82ed25d2a955b2b22f73dca8
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-12 17:04:05 +0100

    Install stimulus

--- rails_application/app/javascript/application.js
+++ rails_application/app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from '@rails/ujs';
-import { Turbo } from "@hotwired/turbo-rails"
-import "channels"
+import Rails from "@rails/ujs";
+import { Turbo } from "@hotwired/turbo-rails";
+import "channels";
+import "controllers";
 
 Rails.start();
--- rails_application/config/importmap.rb
+++ rails_application/config/importmap.rb
@@ -2,6 +2,8 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin_all_from "app/javascript/controllers", under: "controllers"
