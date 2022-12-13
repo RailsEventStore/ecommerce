81747c029515184971537ca5c31f1a1326b01b4c
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-13 07:56:35 +0100

    Get rid off Rails-ujs

--- rails_application/app/javascript/application.js
+++ rails_application/app/javascript/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs";
-import { Turbo } from "@hotwired/turbo-rails";
+import "@hotwired/turbo-rails";
 import "channels";
 import "controllers";
-
-Rails.start();
