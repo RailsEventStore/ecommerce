1320a6bd6d6bcc3c02b9cd11f5e88d13e97d532e
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-14 11:43:00 +0100

    Enhance UpdateTotalOrderTotalValue

--- rails_application/app/read_models/orders/update_order_total_value.rb
+++ rails_application/app/read_models/orders/update_order_total_value.rb
@@ -1,7 +1,7 @@
 module Orders
   class UpdateOrderTotalValue < Infra::EventHandler
     def call(event)
-      order = Order.find_by_uid(event.data.fetch(:order_id))
+      order = Order.find_or_create_by(uid: event.data.fetch(:order_id))
       order.discounted_value = event.data.fetch(:discounted_amount)
       order.total_value = event.data.fetch(:total_amount)
       order.save!
