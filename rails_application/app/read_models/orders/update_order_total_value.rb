f31aed17fd0ccc5338d2db9830c6868bab4b3255
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-16 12:52:00 +0100

    Remember the newest value
    https://blog.arkency.com/read-model-patterns-in-case-of-lack-of-order-guarantee/#remembering_the_newest_value

--- rails_application/app/read_models/orders/update_order_total_value.rb
+++ rails_application/app/read_models/orders/update_order_total_value.rb
@@ -4,18 +4,26 @@ module Orders
       order_id = event.data.fetch(:order_id)
       ApplicationRecord.with_advisory_lock(order_id) do
         order = Order.find_or_create_by!(uid: order_id) { |order| order.state = "Draft" }
-        order.discounted_value = event.data.fetch(:discounted_amount)
-        order.total_value = event.data.fetch(:total_amount)
-        order.save!
 
-        broadcaster.call(order.uid, order.uid, "total_value", number_to_currency(order.total_value))
-        broadcaster.call(order.uid, order.uid, "discounted_value", number_to_currency(order.discounted_value))
+        if is_newest_value?(event, order)
+          order.discounted_value = event.data.fetch(:discounted_amount)
+          order.total_value = event.data.fetch(:total_amount)
+          order.total_value_updated_at = event.metadata.fetch(:timestamp)
+          order.save!
+
+          broadcaster.call(order.uid, order.uid, "total_value", number_to_currency(order.total_value))
+          broadcaster.call(order.uid, order.uid, "discounted_value", number_to_currency(order.discounted_value))
+        end
       end
 
       event_store.link_event_to_stream(event, "Orders$all")
     end
 
     private
+
+    def is_newest_value?(event, order)
+      order.total_value_updated_at.nil? || order.total_value_updated_at < event.metadata.fetch(:timestamp)
+    end
 
     def broadcaster
       Rails.configuration.broadcaster
