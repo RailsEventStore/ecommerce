7772bf6b96576c4c0fd03a217b64704d2d93238d
lukaszreszke <lukaszreszke93@gmail.com>
2022-12-16 12:52:00 +0100

    Remember the newest value
    https://blog.arkency.com/read-model-patterns-in-case-of-lack-of-order-guarantee/#remembering_the_newest_value

--- rails_application/app/read_models/orders/update_discount.rb
+++ rails_application/app/read_models/orders/update_discount.rb
@@ -2,10 +2,18 @@ module Orders
   class UpdateDiscount < Infra::EventHandler
     def call(event)
       order = Order.find_by_uid(event.data.fetch(:order_id))
-      order.percentage_discount = event.data.fetch(:amount)
-      order.save!
+      if is_newest_value?(event, order)
+        order.percentage_discount = event.data.fetch(:amount)
+        order.save!
+      end
 
       event_store.link_event_to_stream(event, "Orders$all")
+    end
+
+    private
+
+    def is_newest_value?(event, order)
+      order.discount_updated_at.nil? || order.discount_updated_at < event.metadata.fetch(:timestamp)
     end
   end
 end
