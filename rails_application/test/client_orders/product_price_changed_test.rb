ace21506ee3446fcd2b2c3b34475c2af4ef2753d
Brychu <pbrych@gmail.com>
2023-01-12 00:21:50 +0100

    Add tests for RegisterLowestPrice

--- rails_application/test/client_orders/product_price_changed_test.rb
+++ rails_application/test/client_orders/product_price_changed_test.rb
@@ -14,6 +14,25 @@ module ClientOrders
       assert_equal 50, Product.find_by_uid(unchanged_product_id).price
     end
 
+    def test_registers_lowest_recent_price
+      product_id = prepare_product
+
+      run_command(Pricing::SetPrice.new(product_id: product_id, price: 40))
+
+      assert_equal 40, Product.find_by_uid(product_id).lowest_recent_price
+    end
+
+    def test_keeps_lowest_recent_price
+      product_id = prepare_product
+
+      run_command(Pricing::SetPrice.new(product_id: product_id, price: 100))
+      run_command(Pricing::SetPrice.new(product_id: product_id, price: 20))
+      run_command(Pricing::SetPrice.new(product_id: product_id, price: 50))
+      run_command(Pricing::SetPrice.new(product_id: product_id, price: 70))
+
+      assert_equal 20, Product.find_by_uid(product_id).lowest_recent_price
+    end
+
     def prepare_product
       product_id = SecureRandom.uuid
       run_command(
