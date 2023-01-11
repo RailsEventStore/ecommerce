3541c39149ce05553b17b63cdb738d96ecbf62c8
Brychu <pbrych@gmail.com>
2023-01-11 23:54:37 +0100

    Register lowest price in ClientOrders::Product read model whenever price is set

---
+++ rails_application/app/read_models/client_orders/register_lowest_price.rb
@@ -0,0 +1,18 @@
+module ClientOrders
+  class RegisterLowestPrice < Infra::EventHandler
+    def call(event)
+      product_id = event.data.fetch(:product_id)
+      product = Product.find(product_id)
+
+      product.update!(lowest_recent_price: lowest_recent_price_for(product_id))
+    end
+
+    private
+
+    def lowest_recent_price_for(product_id)
+      Pricing::PricingCatalog
+        .new(event_store)
+        .lowest_recent_price_by_product_id(product_id)
+    end
+  end
+end
