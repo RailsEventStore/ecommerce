154f1009faa6a21b850090f88e447cf749671bea
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-24 13:28:04 +0100

    Skip use of WithIndifferentAccess transformation
    This means that we can rely on default JSONClient mapper including
    PreserveTypes and SymbolizeMetadataKeys transformations.
    
    It's better to have with_indifferent_access behavior defined in event
    class if one wants to access data or metadata both via string and
    symbol keys.

--- infra/lib/infra/event_store.rb
+++ infra/lib/infra/event_store.rb
@@ -1,37 +1,7 @@
 module Infra
-  class Mapper < RubyEventStore::Mappers::PipelineMapper
-    def initialize
-      super(
-        RubyEventStore::Mappers::Pipeline.new(
-          RubyEventStore::Mappers::Transformation::PreserveTypes
-            .new
-            .register(Symbol, serializer: ->(v) { v.to_s }, deserializer: ->(v) { v.to_sym })
-            .register(
-              Time,
-              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-              deserializer: ->(v) { Time.iso8601(v) }
-            )
-            .register(
-              ActiveSupport::TimeWithZone,
-              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-              deserializer: ->(v) { Time.iso8601(v).in_time_zone },
-              stored_type: ->(*) { "ActiveSupport::TimeWithZone" }
-            )
-            .register(Date, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { Date.iso8601(v) })
-            .register(DateTime, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { DateTime.iso8601(v) })
-            .register(BigDecimal, serializer: ->(v) { v.to_s }, deserializer: ->(v) { BigDecimal(v) }),
-          RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new,
-          RubyEventStore::Transformations::WithIndifferentAccess.new
-        )
-      )
-    end
-  end
-
   class EventStore < SimpleDelegator
-
     def self.main
       new(RailsEventStore::JSONClient.new(
-        mapper: Mapper.new,
         dispatcher: RubyEventStore::ComposedDispatcher.new(
           RailsEventStore::AfterCommitAsyncDispatcher.new(
             scheduler: RubyEventStore::SidekiqScheduler.new(serializer: RubyEventStore::Serializers::YAML)
