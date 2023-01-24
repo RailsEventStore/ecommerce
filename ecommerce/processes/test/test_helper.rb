0b1bea534799866a14319889bface0236bd8e4e2
pjurewicz <pjurewicz93@gmail.com>
2023-01-24 12:50:09 +0100

    fixed Processes test assertions order does matter here

--- ecommerce/processes/test/test_helper.rb
+++ ecommerce/processes/test/test_helper.rb
@@ -17,11 +17,11 @@ module Processes
     end
 
     def assert_command(command)
-      assert_equal(@command_bus.received, command)
+      assert_equal(command, @command_bus.received)
     end
 
     def assert_all_commands(*commands)
-      assert_equal(@command_bus.all_received, commands)
+      assert_equal(commands, @command_bus.all_received)
     end
 
     def assert_no_command
