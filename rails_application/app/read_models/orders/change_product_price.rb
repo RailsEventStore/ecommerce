0dd1d123fb8346aa310cfd1f4fef6d5e6a0e66c7
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2023-01-26 01:07:17 +0100

    Make read models defend against race conditions
    Our read models are now run async which means different orders of events flow can be expected.
    The main trick for now is to use find_or_create.

--- rails_application/app/read_models/orders/change_product_price.rb
+++ rails_application/app/read_models/orders/change_product_price.rb
@@ -1,7 +1,7 @@
 module Orders
   class ChangeProductPrice < Infra::EventHandler
     def call(event)
-      Product.find_by_uid(event.data.fetch(:product_id)).update(price: event.data.fetch(:price))
+      Product.find_or_create_by(uid: event.data.fetch(:product_id)).update(price: event.data.fetch(:price))
 
       event_store.link_event_to_stream(event, "Orders$all")
     end
