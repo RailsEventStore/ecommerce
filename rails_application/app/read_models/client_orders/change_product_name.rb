4b051a4254625283ebfd725a021a001d934f6351
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2023-01-26 01:21:31 +0100

    Revert "Make read models defend against race conditions"
    This reverts commit 0dd1d123fb8346aa310cfd1f4fef6d5e6a0e66c7.

--- rails_application/app/read_models/client_orders/change_product_name.rb
+++ rails_application/app/read_models/client_orders/change_product_name.rb
@@ -1,7 +1,7 @@
 module ClientOrders
   class ChangeProductName < Infra::EventHandler
     def call(event)
-      Product.find_or_create_by(uid: event.data.fetch(:product_id)).update(
+      Product.find_by_uid(event.data.fetch(:product_id)).update(
         name: event.data.fetch(:name)
       )
     end
--- rails_application/test/orders/register_product_test.rb
+++ rails_application/test/orders/register_product_test.rb
@@ -18,21 +18,6 @@ module Orders
       assert event_store.event_in_stream?(product_named.event_id, "Orders$all")
     end
 
-    def test_race_condition
-      product_id = SecureRandom.uuid
-      product_named = ProductCatalog::ProductNamed.new(data: { product_id: product_id, name: "Async Remote" })
-      event_store.publish(product_named)
-
-      product_registered = ProductCatalog::ProductRegistered.new(data: { product_id: product_id })
-      event_store.publish(product_registered)
-
-      product = Product.find_by_uid(product_id)
-      assert product
-      assert_equal "Async Remote", product.name
-      assert event_store.event_in_stream?(product_registered.event_id, "Orders$all")
-      assert event_store.event_in_stream?(product_named.event_id, "Orders$all")
-    end
-
     private
 
     def event_store
