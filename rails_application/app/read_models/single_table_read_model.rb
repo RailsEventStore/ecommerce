024c4254b98075f9571faa80388b89e383fe0cfb
pjurewicz <pjurewicz93@gmail.com>
2023-01-27 17:32:25 +0100

    a generic way to handle asynchronous read model updates

--- rails_application/app/read_models/single_table_read_model.rb
+++ rails_application/app/read_models/single_table_read_model.rb
@@ -20,15 +20,21 @@ class SingleTableReadModel
   end
 
   def create_record(event, id_column)
-    @active_record_name.find_or_create_by(id: event.data.fetch(id_column))
+    concurrent_safely(event) do
+      @active_record_name.find_or_create_by(id: event.data.fetch(id_column))
+    end
   end
 
   def copy_event_attribute_to_column(event, event_attribute, column)
-    find_record(event).update_attribute(column, event.data.fetch(event_attribute))
+    concurrent_safely(event) do
+      find_record(event).update_attribute(column, event.data.fetch(event_attribute))
+    end
   end
 
   def copy_nested_event_attribute_to_column(event, top_event_attribute, nested_attribute, column)
-    find_record(event).update_attribute(column, event.data.fetch(top_event_attribute).fetch(nested_attribute))
+    concurrent_safely(event) do
+      find_record(event).update_attribute(column, event.data.fetch(top_event_attribute).fetch(nested_attribute))
+    end
   end
 
   def find_record(event)
@@ -37,5 +43,23 @@ class SingleTableReadModel
 
   def find(id)
     @active_record_name.find_or_create_by(id: id)
+  end
+
+  private
+
+  def concurrent_safely(event)
+    stream_name = "#{@active_record_name}$#{event.data.fetch(@id_column)}$#{event.event_type}"
+    begin
+      past_events = @event_store.read.stream(stream_name).to_a
+      last_stored = past_events.size - 1
+      @event_store.link(event.event_id, stream_name: stream_name, expected_version: last_stored)
+    rescue RubyEventStore::WrongExpectedEventVersion
+      retry
+    rescue RubyEventStore::EventDuplicatedInStream
+      return
+    else
+      return if past_events.any? && past_events.last.timestamp > event.timestamp
+      yield
+    end
   end
 end
