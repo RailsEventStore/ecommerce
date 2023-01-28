d26e8b51668c1ddd2534352a9b1d93a130c2b748
Brychu <pbrych@gmail.com>
2023-01-28 12:47:42 +0100

    Make sure lowest_recent_price exists in helper method

--- rails_application/app/read_models/public_offer/product.rb
+++ rails_application/app/read_models/public_offer/product.rb
@@ -3,7 +3,7 @@ module PublicOffer
     self.table_name = "public_offer_products"
 
     def lowest_recent_price_lower_from_current?
-      lowest_recent_price < price
+      lowest_recent_price && lowest_recent_price < price
     end
   end
 end
