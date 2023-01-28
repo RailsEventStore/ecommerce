6ec59248ee9d6ec6549193c0a759ad16709cb948
Brychu <pbrych@gmail.com>
2023-01-28 12:25:44 +0100

    Move Product model from configuration to a separate class

---
+++ rails_application/app/read_models/public_offer/product.rb
@@ -0,0 +1,9 @@
+module PublicOffer
+  class Product < ApplicationRecord
+    self.table_name = "public_offer_products"
+
+    def lowest_recent_price_lower_from_current?
+      lowest_recent_price < price
+    end
+  end
+end
