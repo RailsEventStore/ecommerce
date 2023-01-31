1e89e2b0e1a439a0cc6c6f0d0c512c578d2a103a
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2023-01-31 21:41:51 +0100

    search for events

--- build_events_catalog.rb
+++ build_events_catalog.rb
@@ -4,6 +4,7 @@ class BuildEventsCatalog
   PATH = "./"
   SOURCE_PATH = "./ecommerce/"
   EVENTS_CATALOG_DIRECTORY_NAME = "events_catalog"
+  EVENT_TYPE = "Infra::Event"
 
   def call
     recreate_domains_catalog
@@ -11,11 +12,16 @@ class BuildEventsCatalog
       domain.capitalize!
       domain_directory = create_domain_directory(domain)
       create_domain_index(domain, domain_directory)
+      create_domain_events(domain)
     end
   end
 
   def read_domains
     Dir.entries(SOURCE_PATH).select {|entry| File.directory?("#{SOURCE_PATH}#{entry}") and !(entry =='.' || entry == '..' || entry == 'processes') }
+  end
+
+  def source_domain_directory(domain)
+    "#{SOURCE_PATH}#{domain.downcase}"
   end
 
   def recreate_domains_catalog
@@ -51,6 +57,21 @@ class BuildEventsCatalog
     File.open("#{domain_directory}/index.md", "w") do |f|
       f.write(DomainTemplate.new.render(domain))
     end
+  end
+
+  def create_domain_events(domain)
+    domain_events_directory = "#{domains_catalog}/#{domain}/events"
+    FileUtils.mkdir(domain_events_directory)
+    scan_domain_for_events(domain)
+  end
+
+  def scan_domain_for_events(domain)
+    files =  Dir.glob("#{source_domain_path}/**/*").reject { |f| File.directory?(f) || f =='.' || f == '..' }
+    list = files.map do |file|
+      IO.read(file).scan(/class (.*?) < #{EVENT_TYPE}/)
+    end.flatten
+      .reject {|e| e.empty?}
+      .compact
   end
 end
 
