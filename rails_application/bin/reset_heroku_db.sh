4fe3a1d965d8ee5a6883c35fee6714a50e5d2636
pjurewicz <pjurewicz93@gmail.com>
2023-01-31 12:18:00 +0100

    appended sidekiq resetting to reset_heroku_db.sh

--- rails_application/bin/reset_heroku_db.sh
+++ rails_application/bin/reset_heroku_db.sh
@@ -4,3 +4,4 @@ heroku restart --app=res-ecommerce-rails
 heroku pg:reset DATABASE --app=res-ecommerce-rails --confirm res-ecommerce-rails
 heroku run "cd rails_application; rake db:schema:load" --app=res-ecommerce-rails
 heroku run "cd rails_application; rake db:seed" --app=res-ecommerce-rails
+heroku run "cd rails_application; rails runner 'Sidekiq.redis(&:itself).flushdb'" --app=res-ecommerce-rails
