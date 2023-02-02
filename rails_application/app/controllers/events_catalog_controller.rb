238b694af04901ff016cddf631ce9adb8473b187
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2023-02-02 23:14:58 +0100

    events catalog path in env

--- rails_application/app/controllers/events_catalog_controller.rb
+++ rails_application/app/controllers/events_catalog_controller.rb
@@ -1,5 +1,5 @@
 class EventsCatalogController < ApplicationController
   def index
-    render file: Rails.root.join("events_catalog/out/index.html")
+    render file: ENV["EVENTS_CATALOG_PATH"] || "../events_catalog/out/index.html"
   end
 end
