c1e8e622641640634f476c687ae8f8a5e8952b17
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2023-02-07 20:53:47 +0100

    This change silently broke async nature of this handler.
    Revert "Pass command_bus as a param, instead of a global access"
    
    This reverts commit 06d1e0d575a6310d8cf671eb47a5f3cdda62b973.

--- ecommerce/pricing/lib/pricing/calculate_order_total_value.rb
+++ ecommerce/pricing/lib/pricing/calculate_order_total_value.rb
@@ -1,9 +1,14 @@
 module Pricing
   class CalculateOrderTotalValue < Infra::EventHandler
-    def call(event, command_bus)
+    def call(event)
       command_bus.(CalculateTotalValue.new(order_id: event.data.fetch(:order_id)))
     end
 
+    private
+
+    def command_bus
+      Pricing.command_bus
+    end
   end
 end
 
