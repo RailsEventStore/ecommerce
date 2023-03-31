04c87599c90255eeb1c1096f1fe21f435e2be1f2
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-03-31 15:30:51 +0200

    Timezone used here should be domain–specific.
    If shop is run in Poland, it should use Polish timezone to properly
    generate the numbers for Polish accounting rules.

--- ecommerce/ordering/lib/ordering/number_generator.rb
+++ ecommerce/ordering/lib/ordering/number_generator.rb
@@ -1,7 +1,7 @@
 module Ordering
   class NumberGenerator
     def call
-      Time.now.utc.strftime("%Y/%m/#{random_number}")
+      Time.now.strftime("%Y/%m/#{random_number}")
     end
 
     private
--- ecommerce/ordering/test/number_generator_test.rb
+++ ecommerce/ordering/test/number_generator_test.rb
@@ -3,11 +3,11 @@ require_relative "test_helper"
 module Ordering
   class NumberGeneratorTest < Test
     def test_includes_year
-      assert_includes(NumberGenerator.new.call, "#{Time.now.utc.year}")
+      assert_includes(NumberGenerator.new.call, "#{Time.now.year}")
     end
 
     def test_includes_month
-      assert_includes(NumberGenerator.new.call, "#{Time.now.utc.month}")
+      assert_includes(NumberGenerator.new.call, "#{Time.now.month}")
     end
   end
 end
