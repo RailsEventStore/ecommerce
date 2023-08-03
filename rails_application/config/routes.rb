206f7e5b4fe0a1bd29d5647defd46c777d726bd6
bingerxd <zabaw.pat@gmail.com>
2023-08-03 16:14:48 -0400

    Issue 341 and others

--- rails_application/config/routes.rb
+++ rails_application/config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       get :edit_discount
       post :update_discount
       post :reset_discount
+      get :reset_discount
     end
     resource :shipping_address, only: [:edit, :update]
     resource :billing_address, only: [:edit, :update]
