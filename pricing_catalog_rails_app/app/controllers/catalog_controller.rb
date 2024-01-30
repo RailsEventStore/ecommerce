672e2da27d719d34993edfe96057133d4126cff3
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2024-01-30 14:32:13 +0100

    Rails setup for displaying /catalog page at /

---
+++ pricing_catalog_rails_app/app/controllers/catalog_controller.rb
@@ -0,0 +1,5 @@
+class CatalogController < ApplicationController
+  def index
+
+  end
+end
--- pricing_catalog_rails_app/config/routes.rb
+++ pricing_catalog_rails_app/config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "catalog#index"
 end
