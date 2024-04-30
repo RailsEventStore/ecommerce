031bf3b3b18821b61199d1f831942020e992c0d6
pjurewicz <pjurewicz93@gmail.com>
2024-04-30 17:06:17 +0200

    document currently possible order state transitions with test
    1st step towards https://github.com/RailsEventStore/ecommerce/issues/267

--- ecommerce/ordering/test/order_test.rb
+++ ecommerce/ordering/test/order_test.rb
@@ -6,7 +6,7 @@ module Ordering
 
     def setup
       super
-      @order_id   = SecureRandom.uuid
+      @order_id = SecureRandom.uuid
       @product_id = SecureRandom.uuid
       @customer_id = SecureRandom.uuid
     end
@@ -24,7 +24,7 @@ module Ordering
       order.add_item(@product_id)
       order.add_item(@product_id)
       order.submit(NumberGenerator.new.call)
-      assert_equal({@product_id => 2}, order.unpublished_events.to_a.last.data[:order_lines])
+      assert_equal({ @product_id => 2 }, order.unpublished_events.to_a.last.data[:order_lines])
     end
 
     def test_order_lines_after_adding_twice_and_remove_once
@@ -33,7 +33,98 @@ module Ordering
       order.add_item(@product_id)
       order.remove_item(@product_id)
       order.submit(NumberGenerator.new.call)
-      assert_equal({@product_id => 1}, order.unpublished_events.to_a.last.data[:order_lines])
+      assert_equal({ @product_id => 1 }, order.unpublished_events.to_a.last.data[:order_lines])
+    end
+
+    def test_disallowed_order_state_transitions
+      draft_order.submit(NumberGenerator.new.call)
+      assert_raises(Order::InvalidState) { draft_order.accept }
+      assert_raises(Order::InvalidState) { draft_order.reject }
+      assert_raises(Order::InvalidState) { draft_order.cancel }
+      assert_raises(Order::InvalidState) { draft_order.confirm }
+      draft_order.expire
+
+      assert_raises(Order::InvalidState) { submitted_order.submit(NumberGenerator.new.call) }
+      submitted_order.accept
+      submitted_order.reject
+      assert_raises(Order::InvalidState) { submitted_order.cancel }
+      assert_raises(Order::InvalidState) { submitted_order.confirm }
+      submitted_order.expire
+
+      placed_order.submit(NumberGenerator.new.call)
+      assert_raises(Order::InvalidState) { placed_order.accept }
+      assert_raises(Order::InvalidState) { placed_order.reject }
+      placed_order.cancel
+      placed_order.confirm
+      placed_order.expire
+
+      confirmed_order.submit(NumberGenerator.new.call)
+      assert_raises(Order::InvalidState) { confirmed_order.accept }
+      assert_raises(Order::InvalidState) { confirmed_order.reject }
+      assert_raises(Order::InvalidState) { confirmed_order.cancel }
+      assert_raises(Order::InvalidState) { confirmed_order.confirm }
+      assert_raises(Order::InvalidState) { confirmed_order.expire }
+
+      assert_raises(Order::InvalidState) { expired_order.submit(NumberGenerator.new.call) }
+      assert_raises(Order::InvalidState) { rejected_order.accept }
+      assert_raises(Order::InvalidState) { rejected_order.reject }
+      assert_raises(Order::InvalidState) { rejected_order.cancel }
+      assert_raises(Order::InvalidState) { rejected_order.confirm }
+      rejected_order.expire
+
+      assert_raises(Order::InvalidState) { expired_order.submit(NumberGenerator.new.call) }
+      assert_raises(Order::InvalidState) { expired_order.accept }
+      assert_raises(Order::InvalidState) { expired_order.reject }
+      assert_raises(Order::InvalidState) { expired_order.cancel }
+      assert_raises(Order::InvalidState) { expired_order.confirm }
+      expired_order.expire
+
+      cancelled_order.submit(NumberGenerator.new.call)
+      assert_raises(Order::InvalidState) { cancelled_order.accept }
+      assert_raises(Order::InvalidState) { cancelled_order.reject }
+      assert_raises(Order::InvalidState) { cancelled_order.confirm }
+      assert_raises(Order::InvalidState) { cancelled_order.cancel }
+      cancelled_order.expire
+    end
+
+    def draft_order
+      Order.new(@order_id)
+    end
+
+    def submitted_order
+      draft_order.tap do |order|
+        order.submit(NumberGenerator.new.call)
+      end
+    end
+
+    def placed_order
+      submitted_order.tap do |order|
+        order.accept
+      end
+    end
+
+    def rejected_order
+      submitted_order.tap do |order|
+        order.reject
+      end
+    end
+
+    def confirmed_order
+      placed_order.tap do |order|
+        order.confirm
+      end
+    end
+
+    def expired_order
+      draft_order.tap do |order|
+        order.expire
+      end
+    end
+
+    def cancelled_order
+      submitted_order.tap do |order|
+        order.reject
+      end
     end
   end
 end
