RailsEventStore/ecommerce

commit 14101a1750347f15a4aa4b44aaeba55a7a4efb15

Encapsulate Availability read model logic
see 1c2845fdfb6152a7e79c00df5495ceeb514c118e and read
https://blog.arkency.com/offloading-write-side-with-a-read-model/
to see why read model is used in this case

rails_application/app/controllers/client/orders_controller.rb
@@ -32,7 +32,7 @@ module Client
 
     def add_item
       read_model = ClientOrders::OrderLine.where(order_uid: params[:id], product_id: params[:product_id]).first
-      if Availability::Product.exists?(["uid = ? and available < ?", params[:product_id], (read_model&.product_quantity || 0) + 1])
+      unless Availability.approximately_available?(params[:product_id], (read_model&.quantity || 0) + 1)
         redirect_to edit_client_order_path(params[:id]),
                     alert: "Product not available in requested quantity!" and return
       end

rails_application/app/controllers/orders_controller.rb
@@ -55,7 +55,7 @@ class OrdersController < ApplicationController
 
   def add_item
     read_model = Orders::OrderLine.where(order_uid: params[:id], product_id: params[:product_id]).first
-    if Availability::Product.exists?(["uid = ? and available < ?", params[:product_id], (read_model&.quantity || 0) + 1])
+    unless Availability.approximately_available?(params[:product_id], (read_model&.quantity || 0) + 1)
       redirect_to edit_order_path(params[:id]),
                   alert: "Product not available in requested quantity!" and return
     end

rails_application/app/read_models/availability/configuration.rb
@@ -3,6 +3,12 @@ module Availability
     self.table_name = "availability_products"
   end
 
+  # private_constant :Product
+
+  def self.approximately_available?(product_id, desired_quantity)
+    !Product.exists?(["uid = ? and available < ?", product_id, desired_quantity])
+  end
+
   class UpdateAvailability < Infra::EventHandler
     def call(event)
       order = Product.find_or_create_by!(uid: event.data.fetch(:product_id))

rails_application/test/availability/update_availability_test.rb
@@ -10,12 +10,10 @@ module Availability
 
       event_store.publish(Inventory::AvailabilityChanged.new(data: { product_id: product_id, available: 0 }))
 
-      product = Availability::Product.find_by(uid: product_id)
-      assert_equal 0, product.available
+      refute Availability.approximately_available?(product_id, 1)
 
       event_store.publish(Inventory::AvailabilityChanged.new(data: { product_id: product_id, available: 1 }))
-      product.reload
-      assert_equal 1, product.available
+      assert Availability.approximately_available?(product_id, 1)
     end
 
     private

commit ec93d4d8c6bb6f104af16d8505fb20f3c87d2a99

Fix for Adding the product twice crash
Apparently, this area of availability was never covered with tests. Recent changes in https://github.com/RailsEventStore/ecommerce/commit/14101a1750347f15a4aa4b44aaeba55a7a4efb15 introduced a simple bug with column name. This commit fixes this and adds one test to show this.

rails_application/app/controllers/client/orders_controller.rb
@@ -32,7 +32,7 @@ module Client
 
     def add_item
       read_model = ClientOrders::OrderLine.where(order_uid: params[:id], product_id: params[:product_id]).first
-      unless Availability.approximately_available?(params[:product_id], (read_model&.quantity || 0) + 1)
+      unless Availability.approximately_available?(params[:product_id], (read_model&.product_quantity || 0) + 1)
         redirect_to edit_client_order_path(params[:id]),
                     alert: "Product not available in requested quantity!" and return
       end

rails_application/test/integration/client_orders_test.rb
@@ -98,6 +98,19 @@ class ClientOrdersTests < InMemoryRESIntegrationTestCase
     assert_orders_summary("$11.00")
   end
 
+  def test_adding_the_same_product_twice_bug
+    customer_id = register_customer("Customer Shop")
+    product_id = register_product("Fearless Refactoring", 4, 10)
+    Sidekiq::Job.drain_all
+
+    login(customer_id)
+    visit_client_orders
+
+    order_id = SecureRandom.uuid
+    as_client_add_item_to_basket_for_order(product_id, order_id)
+    as_client_add_item_to_basket_for_order(product_id, order_id)
+  end
+
   private
 
   def submit_order_for_customer(customer_id, order_id)