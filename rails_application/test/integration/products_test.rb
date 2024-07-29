954863aeaa6f5136c8bb12a16d320f7d2476f9f6
lukaszreszke <lukaszreszke93@gmail.com>
2024-07-29 10:51:19 +0200

    Cover vat rate

--- rails_application/test/integration/products_test.rb
+++ rails_application/test/integration/products_test.rb
@@ -59,4 +59,59 @@ class ProductsTest < InMemoryRESIntegrationTestCase
     assert_response :unprocessable_entity
     assert_select "span", "Price must be greater than 0"
   end
+
+  def test_does_not_crash_when_vat_rate_is_absent
+    register_customer("Arkency")
+    product_id = SecureRandom.uuid
+
+    get "/products/new"
+    assert_select "h1", "New Product"
+    post "/products",
+         params: {
+           "authenticity_token" => "[FILTERED]",
+           "product_id" => product_id,
+           "name" => "product name",
+           "price" => "100",
+         }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Vat rate can't be blank"
+  end
+
+  def test_does_not_crash_when_vat_rate_is_not_a_number
+    register_customer("Arkency")
+    product_id = SecureRandom.uuid
+
+    get "/products/new"
+    assert_select "h1", "New Product"
+    post "/products",
+         params: {
+           "authenticity_token" => "[FILTERED]",
+           "product_id" => product_id,
+           "name" => "product name",
+           "price" => "100",
+           "vat_rate" =>"abc"
+         }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Vat rate is not a number"
+  end
+
+  def test_does_not_crash_when_name_is_not_present
+    register_customer("Arkency")
+    product_id = SecureRandom.uuid
+
+    get "/products/new"
+    assert_select "h1", "New Product"
+    post "/products",
+         params: {
+           "authenticity_token" => "[FILTERED]",
+           "product_id" => product_id,
+           "price" => "100",
+           "vat_rate" =>"10"
+         }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Name can't be blank"
+  end
 end
