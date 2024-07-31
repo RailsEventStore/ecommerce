25c81433f3ae66a6ccbdf1aedb43e4d77fa48e39
lukaszreszke <lukaszreszke93@gmail.com>
2024-07-31 15:19:05 +0200

    Simplify client

--- infra/lib/infra/event_store.rb
+++ infra/lib/infra/event_store.rb
@@ -1,13 +1,7 @@
 module Infra
   class EventStore < SimpleDelegator
     def self.main
-      new(RailsEventStore::JSONClient.new(
-        dispatcher: RubyEventStore::ComposedDispatcher.new(
-          RailsEventStore::AfterCommitAsyncDispatcher.new(
-            scheduler: RubyEventStore::SidekiqScheduler.new(serializer: RubyEventStore::Serializers::YAML)
-          ),
-          RubyEventStore::Dispatcher.new
-        )))
+      new(RailsEventStore::JSONClient.new)
     end
 
     def self.in_memory
