a8daaff3a967a4a6cb8753eeb5df8418272f90ae
marlena-b <marlena.borowiec01@gmail.com>
2024-08-14 13:49:41 +0200

    BE validation for supply product

---
+++ rails_application/test/integration/supplies_test.rb
@@ -0,0 +1,67 @@
+require "test_helper"
+
+class SuppliesTest < InMemoryRESIntegrationTestCase
+  def test_happy_path
+    product_id = register_product("Async Remote", 100, 10)
+
+    get "/products/#{product_id}/supplies/new"
+    assert_select "h1", "Supply"
+    post "/products/#{product_id}/supplies",
+          params: {
+            "authenticity_token" => "[FILTERED]",
+            "product_id" => product_id,
+            "quantity" => "1"
+          }
+    follow_redirect!
+
+    assert_select "#notice", "Stock level changed"
+  end
+
+  def test_renders_validation_error_when_quantity_is_not_present
+    product_id = register_product("Async Remote", 100, 10)
+
+    get "/products/#{product_id}/supplies/new"
+    assert_select "h1", "Supply"
+    post "/products/#{product_id}/supplies",
+          params: {
+            "authenticity_token" => "[FILTERED]",
+            "product_id" => product_id,
+            "quantity" => ""
+          }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Quantity can't be blank"
+  end
+
+  def test_renders_validation_error_when_quantity_is_not_a_number
+    product_id = register_product("Async Remote", 100, 10)
+
+    get "/products/#{product_id}/supplies/new"
+    assert_select "h1", "Supply"
+    post "/products/#{product_id}/supplies",
+          params: {
+            "authenticity_token" => "[FILTERED]",
+            "product_id" => product_id,
+            "quantity" => "not a number"
+          }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Quantity is not a number"
+  end
+
+  def test_renders_validation_error_when_quantity_is_zero
+    product_id = register_product("Async Remote", 100, 10)
+
+    get "/products/#{product_id}/supplies/new"
+    assert_select "h1", "Supply"
+    post "/products/#{product_id}/supplies",
+          params: {
+            "authenticity_token" => "[FILTERED]",
+            "product_id" => product_id,
+            "quantity" => "0"
+          }
+
+    assert_response :unprocessable_entity
+    assert_select "span", "Quantity must be greater than 0"
+  end
+end
