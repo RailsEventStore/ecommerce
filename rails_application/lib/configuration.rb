5e7c9c3be2cee5432abf7a52d3105892fdc313cb
lukaszreszke <lukaszreszke93@gmail.com>
2024-09-21 11:39:07 +0200

    Fix vat rates

--- rails_application/lib/configuration.rb
+++ rails_application/lib/configuration.rb
@@ -1,8 +1,17 @@
 require_relative "../../infra/lib/infra"
 
+VatRate = Struct.new(:code, :rate)
+
 class Configuration
   def call(event_store, command_bus)
     enable_res_infra_event_linking(event_store)
+  end
+
+  def self.available_vat_rates
+    [
+      VatRate.new("23", 23),
+      VatRate.new("10", 10),
+    ]
   end
 
   private
