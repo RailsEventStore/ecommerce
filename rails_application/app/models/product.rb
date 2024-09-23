7f2ed8c3484002e924b623f740e94935c5e8e3f0
lukaszreszke <lukaszreszke93@gmail.com>
2024-09-23 18:37:12 +0200

    validations

--- rails_application/app/models/product.rb
+++ rails_application/app/models/product.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validate :validate_vat_rate
+  validates :sku, presence: true
+
+  default_scope { where(latest: true) }
+
   before_create :set_stock_level
 
   def set_stock_level
     self.stock_level = 0
+  end
+
+  def validate_vat_rate
+    unless vat_rate.is_a?(Numeric)
+      errors.add(:vat_rate, "is not a number")
+    end
   end
 end
