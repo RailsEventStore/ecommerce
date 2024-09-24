544d9dee65d175edaea3aa2884ae919fec541960
Tomasz Stolarczyk <stolarczykt@gmail.com>
2024-09-24 14:24:24 +0200

    Bring RES tables back.

--- rails_application/db/schema.rb
+++ rails_application/db/schema.rb
@@ -24,6 +24,30 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_091142) do
     t.boolean "vip", default: false
   end
 
+  create_table "event_store_events", id: :serial, force: :cascade do |t|
+    t.uuid "event_id", null: false
+    t.string "event_type", null: false
+    t.jsonb "metadata"
+    t.jsonb "data", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "valid_at", precision: nil
+    t.index ["created_at"], name: "index_event_store_events_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
+    t.index ["event_type"], name: "index_event_store_events_on_event_type"
+    t.index ["valid_at"], name: "index_event_store_events_on_valid_at"
+  end
+
+  create_table "event_store_events_in_streams", id: :serial, force: :cascade do |t|
+    t.string "stream", null: false
+    t.integer "position"
+    t.uuid "event_id", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_in_streams_on_event_id"
+    t.index ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
+    t.index ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
+  end
+
   create_table "invoices_tbl", force: :cascade do |t|
     t.bigint "order_id"
     t.string "order_number"
