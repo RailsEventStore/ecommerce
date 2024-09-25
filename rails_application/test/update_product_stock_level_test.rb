552f91d9b0b2b0b7a6ed0289a7d92b4f641d1ba7
lukaszreszke <lukaszreszke93@gmail.com>
2024-09-25 12:34:11 +0200

    Test UpdateProductStockLevel

---
+++ rails_application/test/update_product_stock_level_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UpdateProductStockLevelTest < InMemoryTestCase
+  def test_happy_path
+    product = Product.create!(name: 'Test Product', price: 10, vat_rate: 23, sku: 'test', stock_level: 0)
+
+    stock_level_increased = Inventory::StockLevelIncreased.new(data: { id: product.id, quantity: 10 })
+    stock_level_increased_second_time = Inventory::StockLevelIncreased.new(data: { id: product.id, quantity: 10 })
+    event_store.append(stock_level_increased, stream_name: "Inventory::Product$#{product.id}")
+    event_store.append(stock_level_increased_second_time, stream_name: "Inventory::Product$#{product.id}")
+    UpdateProductStockLevel.new.call(stock_level_increased)
+    UpdateProductStockLevel.new.call(stock_level_increased_second_time)
+
+    assert_equal 20, product.reload.stock_level
+  end
+
+  private
+
+  def event_store
+    Rails.configuration.event_store
+  end
+end
