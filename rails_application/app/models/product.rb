70e297073edc38314d81007217d9fb83999b4b01
lukaszreszke <lukaszreszke93@gmail.com>
2024-09-25 14:16:59 +0200

    Ignore stock level column of Product

--- rails_application/app/models/product.rb
+++ rails_application/app/models/product.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  self.ignored_columns = %w[stock_level]
+
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }
   validate :validate_vat_rate
@@ -9,12 +11,6 @@ class Product < ApplicationRecord
   has_one :product_catalog, class_name: "Inventory::ProductCatalog", foreign_key: :product_id
 
   default_scope { where(latest: true) }
-
-  before_create :set_stock_level
-
-  def set_stock_level
-    self.stock_level = 0
-  end
 
   def validate_vat_rate
     unless vat_rate.is_a?(Numeric)
