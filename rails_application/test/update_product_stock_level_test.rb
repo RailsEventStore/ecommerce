3a012644aca22651663437fcdbb8c4f938eebb86
lukaszreszke <lukaszreszke93@gmail.com>
2024-09-25 12:34:28 +0200

    Add idempotency test for update_product_stock_level

--- rails_application/test/update_product_stock_level_test.rb
+++ rails_application/test/update_product_stock_level_test.rb
@@ -16,6 +16,17 @@ class UpdateProductStockLevelTest < InMemoryTestCase
     assert_equal 20, product.reload.stock_level
   end
 
+  def test_life_is_brutal_and_full_of_traps
+    product = Product.create!(name: 'Test Product', price: 10, vat_rate: 23, sku: 'test', stock_level: 0)
+
+    stock_level_increased = Inventory::StockLevelIncreased.new(data: { id: product.id, quantity: 10 })
+    event_store.append(stock_level_increased, stream_name: "Inventory::Product$#{product.id}")
+    UpdateProductStockLevel.new.call(stock_level_increased)
+    UpdateProductStockLevel.new.call(stock_level_increased)
+
+    assert_equal 10, product.reload.stock_level
+  end
+
   private
 
   def event_store
