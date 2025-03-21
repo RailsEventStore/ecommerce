1f862274972f9a9b21d02871791cc84d10fe2555
Piotr Romańczuk <porbas@gmail.com>
2025-03-21 13:55:11 +0100

    move order number generation to Fulfillment BC
    It is now an attribute of Fulfillment::OrderRegisterd event

--- ecommerce/fulfillment/lib/fulfillment.rb
+++ ecommerce/fulfillment/lib/fulfillment.rb
@@ -9,11 +9,17 @@ require_relative "fulfillment/on_register_order"
 require_relative "fulfillment/on_cancel_order"
 require_relative "fulfillment/on_confirm_order"
 require_relative "fulfillment/order"
+require_relative "fulfillment/number_generator"
+require_relative "fulfillment/fake_number_generator"
 
 module Fulfillment
   class Configuration
+    def initialize(number_generator)
+      @number_generator = number_generator
+    end
+
     def call(event_store, command_bus)
-      command_bus.register(RegisterOrder, OnRegisterOrder.new(event_store))
+      command_bus.register(RegisterOrder, OnRegisterOrder.new(event_store, @number_generator.call))
       command_bus.register(ConfirmOrder, OnConfirmOrder.new(event_store))
       command_bus.register(CancelOrder, OnCancelOrder.new(event_store))
     end
--- ecommerce/fulfillment/lib/fulfillment/on_register_order.rb
+++ ecommerce/fulfillment/lib/fulfillment/on_register_order.rb
@@ -2,13 +2,15 @@
 
 module Fulfillment
   class OnRegisterOrder
-    def initialize(event_store)
+    def initialize(event_store, number_generator)
       @repository = Infra::AggregateRootRepository.new(event_store)
+      @number_generator = number_generator
     end
 
     def call(command)
       @repository.with_aggregate(Order, command.aggregate_id) do |order|
-        order.register
+        order_number = @number_generator.call
+        order.register(order_number)
       end
     end
   end
