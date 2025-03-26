03738873277779179e0bd09fb5c67ce698fe5be7
Piotr Romańczuk <porbas@gmail.com>
2025-03-26 11:56:03 +0100

    make Ordering test GREEN; temporarily make Ordering tests dependent on Pricing and Fulfillment

--- ecommerce/ordering/test/create_draft_refund_test.rb
+++ ecommerce/ordering/test/create_draft_refund_test.rb
@@ -7,14 +7,23 @@ module Ordering
     def test_draft_refund_created
       order_id = SecureRandom.uuid
       aggregate_id = SecureRandom.uuid
+      product_id = SecureRandom.uuid
       stream = "Ordering::Refund$#{aggregate_id}"
+
+      arrange(
+        Pricing::SetPrice.new(product_id: product_id, price: 11),
+        Pricing::AddPriceItem.new(order_id: order_id, product_id: product_id),
+        Pricing::AddPriceItem.new(order_id: order_id, product_id: product_id),
+        Pricing::AcceptOffer.new(order_id: order_id),
+        Fulfillment::RegisterOrder.new(order_id: order_id),
+      )
 
       expected_events = [
         DraftRefundCreated.new(
           data: {
             refund_id: aggregate_id,
             order_id: order_id,
-            refundable_products: []
+            refundable_products: [{ product_id:, quantity: 2 }]
           }
         )
       ]
