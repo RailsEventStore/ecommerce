b02dd13e7f10598d053db14853b325866b1df42e
Piotr Romańczuk <porbas@gmail.com>
2025-04-10 13:22:15 +0200

    add shipment_process tests

---
+++ ecommerce/processes/test/shipment_process_test.rb
@@ -0,0 +1,57 @@
+require_relative "test_helper"
+
+module Processes
+  class ShipmentProcessTest < Test
+    cover "Processes::ShipmentProcess*"
+
+    def test_submit_and_authorize_shipment_when_order_confirmed_and_address_set
+      given([
+        order_placed,
+        order_confirmed,
+        shipping_address_added],
+        process:
+      )
+
+      assert_all_commands(
+        Shipping::SubmitShipment.new(order_id:),
+        Shipping::AuthorizeShipment.new(order_id:),
+      )
+
+    end
+
+    def test_submit_shipment_when_order_placed_and_address_set
+      given(
+        [order_placed,
+        shipping_address_added].shuffle,
+        process:
+      )
+
+      assert_all_commands(
+        Shipping::SubmitShipment.new(order_id:),
+      )
+    end
+
+    def test_dont_submit_shipment_for_draft_order
+      given([shipping_address_added],
+        process:
+      )
+
+      assert_no_command
+    end
+
+    private
+
+    def process
+      ShipmentProcess.new(event_store, command_bus)
+    end
+
+    def shipping_address_added
+      Shipping::ShippingAddressAddedToShipment.new(
+        data: {
+          order_id: order_id,
+          postal_address: { line_1: "123 Some Street", line_2: "", line_3: "", line_4: "" },
+        }
+      )
+    end
+  end
+end
