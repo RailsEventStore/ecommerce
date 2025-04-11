dbbd6003f7ac8c844757093284386d2ae301df0a
Piotr Romańczuk <porbas@gmail.com>
2025-04-11 09:13:39 +0200

    kill all mutants of ShipmentProcess

--- ecommerce/processes/test/shipment_process_test.rb
+++ ecommerce/processes/test/shipment_process_test.rb
@@ -5,37 +5,39 @@ module Processes
     cover "Processes::ShipmentProcess*"
 
     def test_submit_and_authorize_shipment_when_order_confirmed_and_address_set
-      given([
-        order_placed,
-        order_confirmed,
-        shipping_address_added],
-        process:
-      )
-
+      given([order_placed, order_confirmed, shipping_address_added], process:)
       assert_all_commands(
         Shipping::SubmitShipment.new(order_id:),
         Shipping::AuthorizeShipment.new(order_id:),
       )
-
     end
 
-    def test_submit_shipment_when_order_placed_and_address_set
-      given(
-        [order_placed,
-        shipping_address_added].shuffle,
-        process:
+    def test_submit_shipment_when_order_placed_then_address_set
+      given([order_placed, shipping_address_added], process:)
+      assert_all_commands(
+        Shipping::SubmitShipment.new(order_id:),
       )
+    end
 
+    def test_submit_shipment_when_address_set_then_order_placed
+      given([shipping_address_added, order_placed], process:)
       assert_all_commands(
         Shipping::SubmitShipment.new(order_id:),
       )
     end
 
     def test_dont_submit_shipment_for_draft_order
-      given([shipping_address_added],
-        process:
-      )
+      given([shipping_address_added], process:)
+      assert_no_command
+    end
 
+    def test_need_address_to_submit_shipment
+      given([order_placed], process:)
+      assert_no_command
+    end
+
+    def test_need_address_to_authorize_shipment
+      given([order_placed, order_confirmed], process:)
       assert_no_command
     end
 
