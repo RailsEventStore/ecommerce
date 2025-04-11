eceec2c6655ac40559282fee78f58e3754f14195
Piotr Romańczuk <porbas@gmail.com>
2025-04-11 11:11:26 +0200

    kill mutants in ReservationProcess

--- ecommerce/processes/lib/processes/reservation_process.rb
+++ ecommerce/processes/lib/processes/reservation_process.rb
@@ -39,18 +39,18 @@ module Processes
     def act
       case state
       in order: :accepted
-        update_order_state(state) { reserve_stock(state) }
+        update_order_state { reserve_stock }
       in order: :cancelled
-        release_stock(state, state.reserved_product_ids)
+        release_stock(state.reserved_product_ids)
       in order: :confirmed
-        dispatch_stock(state)
+        dispatch_stock
       else
       end
     end
 
     private
 
-    def reserve_stock(state)
+    def reserve_stock
       unavailable_products = []
       reserved_products = []
       state.order_lines.each do |product_id, quantity|
@@ -61,35 +61,35 @@ module Processes
       end
 
       if unavailable_products.any?
-        release_stock(state, reserved_products)
+        release_stock(reserved_products)
         raise SomeInventoryNotAvailable.new(unavailable_products)
       end
     end
 
-    def update_order_state(state)
+    def update_order_state
       yield
-      accept_order(state)
+      accept_order
     rescue SomeInventoryNotAvailable => exc
-      reject_order(state, exc.unavailable_products)
+      reject_order(exc.unavailable_products)
     end
 
-    def release_stock(state, product_ids)
+    def release_stock(product_ids)
       state.order_lines.slice(*product_ids).each do |product_id, quantity|
         command_bus.(Inventory::Release.new(product_id: product_id, quantity: quantity))
       end
     end
 
-    def dispatch_stock(state)
+    def dispatch_stock
       state.order_lines.each do |product_id, quantity|
         command_bus.(Inventory::Dispatch.new(product_id: product_id, quantity: quantity))
       end
     end
 
-    def accept_order(state)
+    def accept_order
       command_bus.(Fulfillment::RegisterOrder.new(order_id: id))
     end
 
-    def reject_order(state, unavailable_product_ids)
+    def reject_order(unavailable_product_ids)
       command_bus.(Pricing::RejectOffer.new(
         order_id: id, reason: "Some products were unavailable", unavailable_product_ids:)
       )
