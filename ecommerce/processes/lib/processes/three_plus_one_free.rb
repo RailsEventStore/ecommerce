04739fcc64a44411cea1739ad8f63646dcc4c9a1
Piotr Romańczuk <porbas@gmail.com>
2025-04-14 15:07:09 +0200

    kill mutants - WIP
    We need to calculate eligible_free_product *after* event has been applied to state.
    With this modification, we can get rid of the artificial should_act flag in the state

--- ecommerce/processes/lib/processes/three_plus_one_free.rb
+++ ecommerce/processes/lib/processes/three_plus_one_free.rb
@@ -1,19 +1,8 @@
 module Processes
   class ThreePlusOneFree
 
-    class ProcessState < Data.define(:lines, :free_product, :should_act)
-      def initialize(lines: [], free_product: nil, should_act: nil) = super
-
-      MIN_ORDER_LINES_QUANTITY = 4
-
-      def eligible_free_product_id
-        if lines.size >= MIN_ORDER_LINES_QUANTITY
-          line = lines.first
-          line.fetch(:product_id)
-        end
-      end
-
-      def current_free_product_id = free_product
+    class ProcessState < Data.define(:lines, :free_product, :eligible_free_product)
+      def initialize(lines: [], free_product: nil, eligible_free_product: nil) = super
     end
 
     include Infra::ProcessManager.with_state(ProcessState)
@@ -32,37 +21,45 @@ module Processes
       case event
       when Pricing::PriceItemAdded
         lines = (state.lines + [{ product_id:, price: event.data.fetch(:price) }]).sort_by { |line| line.fetch(:price) }
-        state.with(lines:, should_act: true)
+        state.with(lines:, eligible_free_product: eligible_free_product(lines))
       when Pricing::PriceItemRemoved
-        lines = state.lines.dup
+        lines = state.lines
         index_of_line_to_remove = lines.index { |line| line.fetch(:product_id) == product_id && line.fetch(:price) == event.data.fetch(:price) }
         lines.delete_at(index_of_line_to_remove)
-        state.with(lines: lines, should_act: true)
+        state.with(lines:, eligible_free_product: eligible_free_product(lines))
       when Pricing::ProductMadeFreeForOrder
-        state.with(free_product: product_id, should_act: false)
+        state.with(free_product: product_id, eligible_free_product: eligible_free_product(state.lines))
       when Pricing::FreeProductRemovedFromOrder
-        state.with(free_product: nil, should_act: false)
+        state.with(free_product: nil, eligible_free_product: nil)
       end
     end
 
     def act
-      return if state.current_free_product_id == state.eligible_free_product_id
-      return unless !!state.should_act
+      return if state.free_product == state.eligible_free_product
 
-      remove_old_free_product if state.current_free_product_id
-      make_new_product_for_free(state.eligible_free_product_id) if state.eligible_free_product_id
+      remove_old_free_product if state.free_product
+      make_new_product_for_free(state.eligible_free_product) if state.eligible_free_product
     end
 
     def remove_old_free_product
-      command_bus.call(Pricing::RemoveFreeProductFromOrder.new(order_id: id, product_id: state.current_free_product_id))
+      command_bus.call(Pricing::RemoveFreeProductFromOrder.new(order_id: id, product_id: state.free_product))
     end
 
-    def make_new_product_for_free(free_product_id)
-      command_bus.call(Pricing::MakeProductFreeForOrder.new(order_id: id, product_id: free_product_id))
+    def make_new_product_for_free(product_id)
+      command_bus.call(Pricing::MakeProductFreeForOrder.new(order_id: id, product_id: product_id))
     end
 
     def fetch_id(event)
       event.data.fetch(:order_id)
+    end
+
+    MIN_ORDER_LINES_QUANTITY = 4
+
+    def eligible_free_product(lines)
+      if lines.size >= MIN_ORDER_LINES_QUANTITY
+        line = lines.first
+        line.fetch(:product_id)
+      end
     end
   end
 end
