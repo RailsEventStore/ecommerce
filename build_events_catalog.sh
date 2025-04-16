1a0b826abf2c552c870f6d9fa0fbc171f8e62ed8
Piotr Romańczuk <porbas@gmail.com>
2025-04-16 08:34:24 +0200

    use the latest 3.x @eventcatalog/create-eventcatalog

--- build_events_catalog.sh
+++ build_events_catalog.sh
@@ -1,5 +1,5 @@
 rm -rf events_catalog
-npx @eventcatalog/create-eventcatalog@2.2.7 events_catalog
+npx @eventcatalog/create-eventcatalog@3.0.2 events_catalog
 ruby -r "./build_events_catalog.rb" -e "BuildEventsCatalog.new.call"
 cd events_catalog
 npm run build
