40440c3b946c3fa5b032819c9cdda8356bbd7ecb
Piotr Romańczuk <porbas@gmail.com>
2025-04-16 08:35:27 +0200

    make create-eventcatalog command non-interactive
    configure it using CLI switches

--- build_events_catalog.sh
+++ build_events_catalog.sh
@@ -1,5 +1,5 @@
 rm -rf events_catalog
-npx @eventcatalog/create-eventcatalog@3.0.2 events_catalog
+npx @eventcatalog/create-eventcatalog@3.0.2 --organization-name "RailsEventStore" --empty events_catalog
 ruby -r "./build_events_catalog.rb" -e "BuildEventsCatalog.new.call"
 cd events_catalog
 npm run build
