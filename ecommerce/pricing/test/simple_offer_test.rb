1bf74a20e4e58bcdb83db672afcac5c1ebab5967
Piotr Romańczuk <porbas@gmail.com>
2025-04-16 16:09:56 +0200

    add more Pricing::Offer tests to extend coverage and kill mutants

--- ecommerce/pricing/test/simple_offer_test.rb
+++ ecommerce/pricing/test/simple_offer_test.rb
@@ -2,27 +2,125 @@ require_relative "test_helper"
 
 module Pricing
   class SimpleOfferTest < Test
-    cover "Pricing*"
+    cover "Pricing::Offer*"
 
-    def test_removing
-      product_1_id = SecureRandom.uuid
-      set_price(product_1_id, 20)
+    def test_adding
+      product_id = SecureRandom.uuid
+      set_price(product_id, 20)
       order_id = SecureRandom.uuid
-      add_item(order_id, product_1_id)
       stream = "Pricing::Offer$#{order_id}"
       assert_events(
         stream,
+        PriceItemAdded.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            base_price: 20,
+            price: 20,
+            base_total_value: 20,
+            total_value: 20,
+          }
+        ),
         OrderTotalValueCalculated.new(
           data: {
             order_id: order_id,
             discounted_amount: 20,
             total_amount: 20
           }
+        ),
+        PriceItemValueCalculated.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            quantity: 1,
+            amount: 20,
+            discounted_amount: 20,
+          }
         )
-      ) { calculate_total_value(order_id) }
-      remove_item(order_id, product_1_id)
+      ) { add_item(order_id, product_id) }
+
       assert_events(
         stream,
+        PriceItemAdded.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            base_price: 20,
+            price: 20,
+            base_total_value: 40,
+            total_value: 40,
+          }
+        ),
+        OrderTotalValueCalculated.new(
+          data: {
+            order_id: order_id,
+            discounted_amount: 40,
+            total_amount: 40
+          }
+        ),
+        PriceItemValueCalculated.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            quantity: 2,
+            amount: 40,
+            discounted_amount: 40,
+          }
+        )
+      ) { add_item(order_id, product_id) }
+
+    end
+
+    def test_removing
+      product_id = SecureRandom.uuid
+      set_price(product_id, 20)
+      order_id = SecureRandom.uuid
+      add_item(order_id, product_id)
+      add_item(order_id, product_id)
+      stream = "Pricing::Offer$#{order_id}"
+
+      assert_events(
+        stream,
+        PriceItemRemoved.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            base_price: 20,
+            price: 20,
+            base_total_value: 20,
+            total_value: 20,
+          }
+        ),
+        OrderTotalValueCalculated.new(
+          data: {
+            order_id: order_id,
+            discounted_amount: 20,
+            total_amount: 20
+          }
+        ),
+        PriceItemValueCalculated.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            quantity: 1,
+            amount: 20,
+            discounted_amount: 20,
+          }
+        )
+      ) { remove_item(order_id, product_id) }
+
+      assert_events(
+        stream,
+        PriceItemRemoved.new(
+          data: {
+            order_id: order_id,
+            product_id: product_id,
+            base_price: 20,
+            price: 20,
+            base_total_value: 0,
+            total_value: 0,
+          }
+        ),
         OrderTotalValueCalculated.new(
           data: {
             order_id: order_id,
@@ -30,7 +128,7 @@ module Pricing
             total_amount: 0
           }
         )
-      ) { calculate_total_value(order_id) }
+      ) { remove_item(order_id, product_id) }
     end
   end
 end
