41fb18494af038efeef779657c52b6f4a18eb634
lukaszreszke <lukaszreszke93@gmail.com>
2025-05-29 19:21:26 +0200

    Revert "Kill mutant"
    This reverts commit dd476988f754ff053bb1d402cf3f08ee4995c2e6.

--- ecommerce/pricing/test/multiple_promotion_test.rb
+++ ecommerce/pricing/test/multiple_promotion_test.rb
@@ -61,65 +61,6 @@ module Pricing
       ) { run_command(RemovePercentageDiscount.new(order_id: order_id)) }
     end
 
-    
-def test_given_multiple_discounts_applied_when_time_promotion_is_removed_then_first_promotion_is_applied
-  order_id = SecureRandom.uuid
-  product_id = SecureRandom.uuid
-
-  given(
-    [
-      TimePromotionCreated.new(
-        data: {
-          time_promotion_id: SecureRandom.uuid,
-          discount: 10,
-          start_time: Time.current - 1,
-          end_time: Time.current + 1
-        }
-      ),
-      Pricing::PriceItemAdded.new(
-        data: {
-          product_id: product_id,
-          order_id: order_id,
-          base_price: 1000,
-          price: 1000,
-          base_total_value: 1000,
-          total_value: 1000
-        }
-      ),
-      PercentageDiscountSet.new(
-        data: {
-          order_id: order_id,
-          type: Discounts::TIME_PROMOTION_DISCOUNT,
-          amount: 10,
-          base_total_value: 1000,
-          total_value: 900
-        }
-      ),
-      PercentageDiscountSet.new(
-        data: {
-          order_id: order_id,
-          type: Discounts::GENERAL_DISCOUNT,
-          amount: 50,
-          base_total_value: 1000,
-          total_value: 450
-        }
-      )
-    ]
-  )
-
-  assert_events_contain(
-    stream_name(order_id),
-      PercentageDiscountRemoved.new(
-        data: {
-          order_id: order_id,
-          type: Discounts::TIME_PROMOTION_DISCOUNT,
-          base_total_value: 1000,
-          total_value: 500
-        }
-      ),
-  ) { run_command(RemoveTimePromotionDiscount.new(order_id: order_id)) }
-end
-
     private
 
     def given(events)
