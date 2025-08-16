b1e509ff3915162318b79c3a4b7c066bab59fadb
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2025-08-16 13:01:46 +0200

    TotalOrderValue uses Infra::ProcessManager
    This module is just too handy not to use it.
    However, the name might be confusing - it's a SummaryEventBuilder. Maybe in the future we may want to create a separate module for this.

--- rails_application/app/processes/processes/total_order_value.rb
+++ rails_application/app/processes/processes/total_order_value.rb
@@ -1,27 +1,49 @@
 module Processes
   class TotalOrderValue
-    def initialize(event_store)
-      @event_store = event_store
-      @total_value = 0
+    include Infra::ProcessManager.with_state { Offer }
+
+    subscribes_to(
+      Pricing::PriceItemAdded
+    )
+
+    private
+
+    def act
+      @total_value = state.lines.sum { |line| line.fetch(:price) }
+      publish_total_order_value
     end
 
-    def call(event)
+    def apply(event)
+      @order_id = event.data.fetch(:order_id)
       case event
       when Pricing::PriceItemAdded
-        @total_value += event.data.fetch(:price)
-        @order_id     = event.data.fetch(:order_id)
+        lines = (state.lines + [{ price: event.data.fetch(:price) }])
+        state.with(lines:)
+      else
+        state
       end
-      publish_total_order_value
     end
 
     private
 
     def publish_total_order_value
-      @event_store.publish(
+      event_store.publish(
         TotalOrderValueUpdated.new(data: { total_value: @total_value, order_id: @order_id }),
         stream_name: "Processes::TotalOrderValue$#{@order_id}"
       )
     end
+
+    def fetch_id(event)
+      event.data.fetch(:order_id)
+    end
   end
+
+  Offer = Data.define(:lines) do
+    def initialize(lines: [])
+      super(lines: lines.freeze)
+    end
+  end
+
+
 
 end
--- rails_application/test/processes/total_order_value_test.rb
+++ rails_application/test/processes/total_order_value_test.rb
@@ -4,32 +4,54 @@ module Processes
   class TotalOrderValueTest < ProcessTest
     cover "Processes::TotalOrderValue"
 
-    def test_total_order_value_calculation
-      process = TotalOrderValue.new(event_store)
+  def test_total_order_value_calculation_for_1_item
+    process = TotalOrderValue.new(event_store, command_bus)
+    event = price_item_added
+    
+    event_store.append(event)
 
-      assert_events_contain(
-        "Processes::TotalOrderValue$#{price_item_added.data[:order_id]}",
-        Processes::TotalOrderValueUpdated.new(
-          data: {
-            total_value: 100,
-            order_id: order_id
-          }
-        )) do
-          process.call(price_item_added)
-        end
+    assert_events_contain(
+      "Processes::TotalOrderValue$#{event.data[:order_id]}",
+      Processes::TotalOrderValueUpdated.new(
+        data: {
+          total_value: 100,
+          order_id: order_id
+        }
+      )) do
+        process.call(event)
+      end
+  end
 
+  def test_total_order_value_calculation_for_2_items_of_the_same_product
+    process = TotalOrderValue.new(event_store, command_bus)
+    product_id = SecureRandom.uuid
+    event_1 = price_item_added(product_id)
+    event_2 = price_item_added(product_id)
 
+    event_store.append(event_1)
+    event_store.append(event_2)
 
+    process.call(event_1)
+
+    assert_events_contain(
+      "Processes::TotalOrderValue$#{event_2.data[:order_id]}",
+      Processes::TotalOrderValueUpdated.new(
+        data: {
+          total_value: 200,
+          order_id: order_id
+        }
+      )) do
+      process.call(event_2)
     end
+  end
 
     private
 
-    def price_item_added
+    def price_item_added(product_id = SecureRandom.uuid)
       Pricing::PriceItemAdded.new(data: {
         order_id: order_id,
-        item_id: SecureRandom.uuid,
         price: 100,
-        product_id: SecureRandom.uuid,
+        product_id: product_id,
         base_price: 100,
         base_total_value: 100,
         total_value: 100
