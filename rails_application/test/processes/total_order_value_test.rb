c20bff77ba998291d251853b743d9aeb40110e81
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2025-08-18 00:41:34 +0200

    TotalOrderValue - new test for 2 different products

--- rails_application/test/processes/total_order_value_test.rb
+++ rails_application/test/processes/total_order_value_test.rb
@@ -4,46 +4,72 @@ module Processes
   class TotalOrderValueTest < ProcessTest
     cover "Processes::TotalOrderValue"
 
-  def test_total_order_value_calculation_for_1_item
-    process = TotalOrderValue.new(event_store, command_bus)
-    event = price_item_added
-    
-    event_store.append(event)
+    def test_total_order_value_calculation_for_1_item
+      process = TotalOrderValue.new(event_store, command_bus)
+      event = price_item_added
 
-    assert_events_contain(
-      "Processes::TotalOrderValue$#{event.data[:order_id]}",
-      Processes::TotalOrderValueUpdated.new(
-        data: {
-          total_value: 100,
-          order_id: order_id
-        }
-      )) do
-        process.call(event)
-      end
-  end
+      event_store.append(event)
 
-  def test_total_order_value_calculation_for_2_items_of_the_same_product
-    process = TotalOrderValue.new(event_store, command_bus)
-    product_id = SecureRandom.uuid
-    event_1 = price_item_added(product_id)
-    event_2 = price_item_added(product_id)
-
-    event_store.append(event_1)
-    event_store.append(event_2)
-
-    process.call(event_1)
-
-    assert_events_contain(
-      "Processes::TotalOrderValue$#{event_2.data[:order_id]}",
-      Processes::TotalOrderValueUpdated.new(
-        data: {
-          total_value: 200,
-          order_id: order_id
-        }
-      )) do
-      process.call(event_2)
+      assert_events_contain(
+        "Processes::TotalOrderValue$#{event.data[:order_id]}",
+        Processes::TotalOrderValueUpdated.new(
+          data: {
+            total_value: 100,
+            order_id: order_id
+          }
+        )) do
+          process.call(event)
+        end
     end
-  end
+
+    def test_total_order_value_calculation_for_2_items_of_the_same_product
+      process = TotalOrderValue.new(event_store, command_bus)
+      product_id = SecureRandom.uuid
+      event_1 = price_item_added(product_id)
+      event_2 = price_item_added(product_id)
+
+      event_store.append(event_1)
+      event_store.append(event_2)
+
+      process.call(event_1)
+
+      assert_events_contain(
+        "Processes::TotalOrderValue$#{event_2.data[:order_id]}",
+        Processes::TotalOrderValueUpdated.new(
+          data: {
+            total_value: 200,
+            order_id: order_id
+          }
+        )) do
+        process.call(event_2)
+      end
+    end
+
+    def test_total_order_value_calculation_for_2_items_of_different_products
+      process = TotalOrderValue.new(event_store, command_bus)
+      product_id_1 = SecureRandom.uuid
+      product_id_2 = SecureRandom.uuid
+      event_1 = price_item_added(product_id_1)
+      event_2 = price_item_added(product_id_2)
+
+      event_store.append(event_1)
+      event_store.append(event_2)
+
+      process.call(event_1)
+
+      assert_events_contain(
+        "Processes::TotalOrderValue$#{event_2.data[:order_id]}",
+        Processes::TotalOrderValueUpdated.new(
+          data: {
+            total_value: 200,
+            order_id: order_id
+          }
+        )) do
+        process.call(event_2)
+      end
+    end
+
+
 
     private
 
