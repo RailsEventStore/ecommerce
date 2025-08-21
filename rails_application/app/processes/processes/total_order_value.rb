90d54e8e42f1d7c85c7d7d76ec2d389a32ba384a
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2025-08-22 00:50:58 +0200

    Refactored TotalOrderValue to rely more on private methods

--- rails_application/app/processes/processes/total_order_value.rb
+++ rails_application/app/processes/processes/total_order_value.rb
@@ -13,47 +13,26 @@ module Processes
     private
 
     def act
-      subtotal = state.lines.sum { |line| line.fetch(:price) }
-      
-      total_discount = state.discounts.sum { |discount| discount.fetch(:amount) }
-      final_discount = [total_discount, 100].min
-      
-      discounted_value = subtotal * (1 - final_discount / 100.0)
-      publish_total_order_value(subtotal, discounted_value)
+      publish_total_order_value(calculate_subtotal, calculate_discounted_value)
     end
 
     def apply(event)
       @order_id = event.data.fetch(:order_id)
       case event
       when Pricing::PriceItemAdded
-        product_id = event.data.fetch(:product_id)
-        base_price = event.data.fetch(:base_price)
-        lines = (state.lines + [{ product_id:, price: base_price }])
-        state.with(lines:)
+        apply_price_item_added(event)
       when Pricing::PriceItemRemoved
-        lines = state.lines.reject { |line| line.fetch(:product_id) == event.data.fetch(:product_id) }
-        state.with(lines:)
+        apply_price_item_removed(event)
       when Pricing::PercentageDiscountSet
-        discount_type = event.data.fetch(:type)
-        discount_amount = event.data.fetch(:amount)
-        discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
-        discounts = discounts + [{ type: discount_type, amount: discount_amount }]
-        state.with(discounts:)
+        apply_percentage_discount_set(event)
       when Pricing::PercentageDiscountChanged
-        discount_type = event.data.fetch(:type)
-        discount_amount = event.data.fetch(:amount)
-        discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
-        discounts = discounts + [{ type: discount_type, amount: discount_amount }]
-        state.with(discounts:)
+        apply_percentage_discount_changed(event)
       when Pricing::PercentageDiscountRemoved
-        discount_type = event.data.fetch(:type)
-        discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
-        state.with(discounts:)
+        apply_percentage_discount_removed(event)
       else
         state
       end
     end
-
     private
 
     def publish_total_order_value(total_amount, discounted_amount)
@@ -69,6 +48,56 @@ module Processes
 
     def fetch_id(event)
       event.data.fetch(:order_id)
+    end
+
+    def apply_price_item_added(event)
+      product_id = event.data.fetch(:product_id)
+      base_price = event.data.fetch(:base_price)
+      lines = (state.lines + [{ product_id:, price: base_price }])
+      state.with(lines:)
+    end
+
+    def apply_price_item_removed(event)
+      lines = state.lines.reject { |line| line.fetch(:product_id) == event.data.fetch(:product_id) }
+      state.with(lines:)
+    end
+
+    def apply_percentage_discount_set(event)
+      discount_type = event.data.fetch(:type)
+      discount_amount = event.data.fetch(:amount)
+      discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
+      discounts = discounts + [{ type: discount_type, amount: discount_amount }]
+      state.with(discounts:)
+    end
+
+    def apply_percentage_discount_changed(event)
+      discount_type = event.data.fetch(:type)
+      discount_amount = event.data.fetch(:amount)
+      discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
+      discounts = discounts + [{ type: discount_type, amount: discount_amount }]
+      state.with(discounts:)
+    end
+
+    def apply_percentage_discount_removed(event)
+      discount_type = event.data.fetch(:type)
+      discounts = state.discounts.reject { |d| d.fetch(:type) == discount_type }
+      state.with(discounts:)
+    end
+
+    def calculate_subtotal
+      state.lines.sum { |line| line.fetch(:price) }
+    end
+
+    def calculate_total_discount_percentage
+      state.discounts.sum { |discount| discount.fetch(:amount) }
+    end
+
+    def calculate_final_discount_percentage
+      [calculate_total_discount_percentage, 100].min
+    end
+
+    def calculate_discounted_value
+      calculate_subtotal * (1 - calculate_final_discount_percentage / 100.0)
     end
   end
 
