6a542752118c8374aec5484f89372cc5a8e272db
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2025-09-10 12:26:40 +0200

    Debug CI issues.

--- infra/lib/infra/event_store.rb
+++ infra/lib/infra/event_store.rb
@@ -2,8 +2,10 @@ module Infra
   class EventStore < SimpleDelegator
     def self.main
       if ENV['DISABLE_EVENT_TRANSFORMATIONS'] == 'true'
-        mapper = default_mapper
+        puts "[DEBUG] Event transformations DISABLED"
+        mapper = debug_mapper
       else
+        puts "[DEBUG] Event transformations ENABLED"
         require_relative "../../../rails_application/lib/transformations/refund_to_return_event_mapper" rescue nil
 
         if defined?(Transformations::RefundToReturnEventMapper)
@@ -24,7 +26,21 @@ module Infra
         end
       end
 
-      new(RailsEventStore::JSONClient.new(mapper: mapper))
+      client = RailsEventStore::JSONClient.new(mapper: mapper)
+
+      def client.publish(*events, **kwargs)
+        events.each do |event|
+          if event.respond_to?(:timestamp) && event.timestamp.nil?
+            puts "[ERROR] Event #{event.class.name} has nil timestamp!"
+            puts "[ERROR] Event ID: #{event.event_id}"
+            puts "[ERROR] Stack trace:"
+            puts caller[0..10].join("\n")
+          end
+        end
+        super(*events, **kwargs)
+      end
+
+      new(client)
     end
 
     def self.in_memory
@@ -56,6 +72,29 @@ module Infra
     end
 
     private
+
+    def self.debug_mapper
+      default_mapper_instance = default_mapper
+
+      class << default_mapper_instance
+        alias_method :original_event_to_record, :event_to_record
+
+        def event_to_record(domain_event)
+          record = original_event_to_record(domain_event)
+          if record.timestamp.nil?
+            puts "[ERROR] Record created with nil timestamp!"
+            puts "[ERROR] Event class: #{domain_event.class.name}"
+            puts "[ERROR] Event ID: #{domain_event.event_id}"
+            puts "[ERROR] Domain event timestamp: #{domain_event.respond_to?(:timestamp) ? domain_event.timestamp : 'N/A'}"
+            puts "[ERROR] Stack trace:"
+            puts caller[0..15].join("\n")
+          end
+          record
+        end
+      end
+
+      default_mapper_instance
+    end
 
     def self.default_mapper
       RubyEventStore::Mappers::PipelineMapper.new(
