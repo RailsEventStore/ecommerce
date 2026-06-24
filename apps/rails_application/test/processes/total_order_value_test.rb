11a56cc42565956187c23c96c08d2291d7ef2f13
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-06-24 20:48:47 +0200

    Revert "Drop ensure_supported_any_usage: keyword removed in RES 3.0"
    This reverts commit 9e1f8dac92e6a47cb8b7358f9a4f82c5222fecd7.

--- apps/rails_application/test/processes/total_order_value_test.rb
+++ apps/rails_application/test/processes/total_order_value_test.rb
@@ -576,7 +576,7 @@ module Processes
 
     def test_publishes_with_consistent_stream_versioning
       store = RubyEventStore::Client.new(
-        repository: RubyEventStore::InMemoryRepository.new
+        repository: RubyEventStore::InMemoryRepository.new(ensure_supported_any_usage: true)
       )
       process = TotalOrderValue.new(store, command_bus)
       event = price_item_added
